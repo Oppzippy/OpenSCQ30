bf5da610de08c17b8d49a36ada35487200c18d01
Kyle Scheuing <kyle@kylescheuing.com>
2022-11-25 22:32:28 -0500

    lib: add Send trait to errors

--- lib/src/soundcore_bluetooth/traits/soundcore_device_connection_error.rs
+++ lib/src/soundcore_bluetooth/traits/soundcore_device_connection_error.rs
@@ -5,19 +5,22 @@ use uuid::Uuid;
 #[derive(thiserror::Error, Debug)]
 pub enum SoundcoreDeviceConnectionError {
     #[error("device not found: {source}")]
-    DeviceNotFound { source: Box<dyn Error> },
+    DeviceNotFound { source: Box<dyn Error + Send> },
 
     #[error("not connected: {source}")]
-    NotConnected { source: Box<dyn Error> },
+    NotConnected { source: Box<dyn Error + Send> },
 
     #[error("name of device with mac address `{mac_address}` not found")]
     NameNotFound { mac_address: String },
 
     #[error("characteristic `{uuid}` not found: {source}")]
-    CharacteristicNotFound { uuid: Uuid, source: Box<dyn Error> },
+    CharacteristicNotFound {
+        uuid: Uuid,
+        source: Box<dyn Error + Send>,
+    },
 
     #[error(transparent)]
-    Other { source: Box<dyn Error> },
+    Other { source: Box<dyn Error + Send> },
 
     #[error("device didn't respond to request")]
     NoResponse,
