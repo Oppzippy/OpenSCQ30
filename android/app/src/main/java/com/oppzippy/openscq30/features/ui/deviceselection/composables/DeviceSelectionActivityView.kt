7155f21a8a0bb5088d209bd3227be78e76647276
Kyle Scheuing <kyle@kylescheuing.com>
2023-01-31 18:30:37 -0500

    android: add accompanist-permissions for permission handling
    This commit also fixes permissions on api versions <31

--- android/app/src/main/java/com/oppzippy/openscq30/features/ui/deviceselection/composables/DeviceSelectionActivityView.kt
+++ android/app/src/main/java/com/oppzippy/openscq30/features/ui/deviceselection/composables/DeviceSelectionActivityView.kt
@@ -2,30 +2,36 @@ package com.oppzippy.openscq30.features.ui.deviceselection.composables
 
 import android.Manifest
 import android.content.Intent
+import android.os.Build
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
 import com.oppzippy.openscq30.features.ui.deviceselection.models.BluetoothDeviceProvider
 import com.oppzippy.openscq30.features.ui.devicesettings.DeviceSettingsActivity
 import com.oppzippy.openscq30.ui.theme.OpenSCQ30Theme
+import com.oppzippy.openscq30.R
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun DeviceSelectionActivityView(
     bluetoothDeviceProvider: BluetoothDeviceProvider
 ) {
-    var devices by remember { mutableStateOf(bluetoothDeviceProvider.getDevices()) }
-
-    val launcher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.RequestPermission(),
-    ) { isGranted ->
-        if (isGranted) {
-            devices = bluetoothDeviceProvider.getDevices()
-        }
+    val permissionState = if (Build.VERSION.SDK_INT >= 31) {
+        rememberPermissionState(Manifest.permission.BLUETOOTH_CONNECT)
+    } else {
+        rememberPermissionState(Manifest.permission.BLUETOOTH)
     }
 
     val context = LocalContext.current
@@ -34,13 +40,37 @@ fun DeviceSelectionActivityView(
         Surface(
             modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
         ) {
-            DeviceSelection(devices, onRefreshClick = {
-                launcher.launch(Manifest.permission.BLUETOOTH)
-            }, onDeviceClick = { device ->
-                val intent = Intent(context, DeviceSettingsActivity::class.java)
-                intent.putExtra("macAddress", device.address)
-                context.startActivity(intent)
-            })
+            if (!permissionState.status.isGranted) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxHeight(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Text(stringResource(R.string.bluetooth_permission_is_required))
+                        Button(onClick = { permissionState.launchPermissionRequest() }) {
+                            Text(stringResource(R.string.request_permission))
+                        }
+                    }
+                }
+            } else {
+                var devices by remember { mutableStateOf(bluetoothDeviceProvider.getDevices()) }
+                DeviceSelection(
+                    devices,
+                    onRefreshClick = {
+                        devices = bluetoothDeviceProvider.getDevices()
+                    },
+                    onDeviceClick = { device ->
+                        val intent = Intent(context, DeviceSettingsActivity::class.java)
+                        intent.putExtra("macAddress", device.address)
+                        context.startActivity(intent)
+                    },
+                )
+            }
         }
     }
 
--- android/app/src/main/java/com/oppzippy/openscq30/features/ui/deviceselection/models/BluetoothDeviceProvider.kt
+++ android/app/src/main/java/com/oppzippy/openscq30/features/ui/deviceselection/models/BluetoothDeviceProvider.kt
@@ -1,6 +1,7 @@
 package com.oppzippy.openscq30.features.ui.deviceselection.models
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
@@ -10,22 +11,19 @@ import androidx.core.app.ActivityCompat
 import com.oppzippy.openscq30.lib.SoundcoreDeviceUtils
 
 class BluetoothDeviceProvider(private val context: Context) {
+    /**
+     * The caller is responsible for checking for bluetooth permission
+     */
+    @SuppressLint("MissingPermission")
     fun getDevices(): List<BluetoothDeviceModel> {
         val bluetoothManager: BluetoothManager =
             context.getSystemService(BluetoothManager::class.java)
         val adapter: BluetoothAdapter? = bluetoothManager.adapter
         if (adapter != null) {
-            if (ActivityCompat.checkSelfPermission(
-                    context, Manifest.permission.BLUETOOTH_CONNECT
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                return adapter.bondedDevices.filter {
-                    SoundcoreDeviceUtils.isMacAddressSoundcoreDevice(it.address)
-                }.map {
-                    BluetoothDeviceModel(it.name, it.address)
-                }
-            } else {
-                Log.w("device-selection", "no permission")
+            return adapter.bondedDevices.filter {
+                SoundcoreDeviceUtils.isMacAddressSoundcoreDevice(it.address)
+            }.map {
+                BluetoothDeviceModel(it.name, it.address)
             }
         } else {
             Log.w("device-selection", "no bluetooth adapter")
