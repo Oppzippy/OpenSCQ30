7d5248c702fb16b45b299338110e8c8669da6c82
Kyle Scheuing <kyle@kylescheuing.com>
2023-02-13 16:03:18 -0500

    android: upgrade glib-build-tools to 0.17

--- gui/build.rs
+++ gui/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     glib_build_tools::compile_resources(
-        "src/widgets",
+        &["src/widgets"],
         "src/widgets/widgets.gresource.xml",
         "widgets.gresource",
     );
