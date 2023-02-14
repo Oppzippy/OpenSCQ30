93a55f62a114a3b1b7389c40f20c3046cf199ed5
Kyle Scheuing <kyle@kylescheuing.com>
2023-02-14 15:26:42 -0500

    lib: simplify bluetooth feature cfg

--- lib/src/soundcore_bluetooth.rs
+++ lib/src/soundcore_bluetooth.rs
@@ -1,22 +1,17 @@
 pub mod stub;
 pub mod traits;
 
-#[cfg(all(
-    feature = "bluetooth",
-    any(target_os = "windows", target_os = "macos", target_os = "linux")
-))]
+#[cfg(feature = "bluetooth")]
 use self::traits::{SoundcoreDeviceConnectionError, SoundcoreDeviceConnectionRegistry};
 
-#[cfg(all(
-    feature = "bluetooth",
-    any(target_os = "windows", target_os = "macos", target_os = "linux")
-))]
+#[cfg(feature = "bluetooth")]
 pub(crate) mod btleplug;
-#[cfg(all(
-    feature = "bluetooth",
-    any(target_os = "windows", target_os = "macos", target_os = "linux")
-))]
+
+#[cfg(feature = "bluetooth")]
 pub async fn new_connection_registry(
 ) -> Result<impl SoundcoreDeviceConnectionRegistry, SoundcoreDeviceConnectionError> {
-    btleplug::new_connection_registry().await
+    #[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
+    {
+        btleplug::new_connection_registry().await
+    }
 }
