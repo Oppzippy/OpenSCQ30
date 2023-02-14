df81c6566a7078ab70363823a1721dcbb7fa9c9e
Kyle Scheuing <kyle@kylescheuing.com>
2023-02-14 13:53:45 -0500

    lib: rename btleplug feature to bluetooth
    This way the implementation can change depending on platform using the
    same feature name.

--- lib/src/soundcore_bluetooth.rs
+++ lib/src/soundcore_bluetooth.rs
@@ -1,12 +1,21 @@
-#[cfg(feature = "btleplug")]
-pub(crate) mod btleplug;
 pub mod stub;
 pub mod traits;
 
-#[cfg(feature = "btleplug")]
+#[cfg(all(
+    feature = "bluetooth",
+    any(target_os = "windows", target_os = "macos", target_os = "linux")
+))]
 use self::traits::{SoundcoreDeviceConnectionError, SoundcoreDeviceConnectionRegistry};
 
-#[cfg(feature = "btleplug")]
+#[cfg(all(
+    feature = "bluetooth",
+    any(target_os = "windows", target_os = "macos", target_os = "linux")
+))]
+pub(crate) mod btleplug;
+#[cfg(all(
+    feature = "bluetooth",
+    any(target_os = "windows", target_os = "macos", target_os = "linux")
+))]
 pub async fn new_connection_registry(
 ) -> Result<impl SoundcoreDeviceConnectionRegistry, SoundcoreDeviceConnectionError> {
     btleplug::new_connection_registry().await
