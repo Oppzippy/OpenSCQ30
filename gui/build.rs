220768fa405d04a23e34ecf657eda1c49e5b9c20
Kyle Scheuing <kyle@kylescheuing.com>
2023-02-24 21:55:20 -0500

    gui: add icon on windows

--- gui/build.rs
+++ gui/build.rs
@@ -1,7 +1,18 @@
 fn main() {
+    #[cfg(target_os = "windows")]
+    windows();
+
     glib_build_tools::compile_resources(
         &["src/widgets"],
         "src/widgets/widgets.gresource.xml",
         "widgets.gresource",
     );
+}
+
+#[cfg(target_os = "windows")]
+fn windows() {
+    use std::path::Path;
+
+    let resource_dir = Path::new("resources");
+    embed_resource::compile(resource_dir.join("resources.rc"));
 }
