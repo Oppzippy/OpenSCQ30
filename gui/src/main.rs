62e705d1e962f6ee59938702c5526de0251ad7c7
Kyle Scheuing <kyle@kylescheuing.com>
2023-03-09 17:14:39 -0500

    gui: call gtk::init() in tests

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -44,6 +44,7 @@ fn main() {
 static LOAD_RESOURCES: Once = Once::new();
 
 pub fn load_resources() {
+    gtk::init().unwrap();
     LOAD_RESOURCES.call_once(|| {
         gio::resources_register_include!("widgets.gresource").expect("failed to load widgets");
     });
