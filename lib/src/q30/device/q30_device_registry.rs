318976077a916ead77b53de9c415ca907ed2e4b7
Kyle Scheuing <kyle@kylescheuing.com>
2023-03-11 16:07:21 -0500

    test(lib): add tests for Q30DeviceRegistry

--- lib/src/q30/device/q30_device_registry.rs
+++ lib/src/q30/device/q30_device_registry.rs
@@ -77,3 +77,110 @@ where
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{collections::HashMap, sync::Arc};
+
+    use tokio::sync::mpsc;
+
+    use crate::{
+        api::{
+            connection::ConnectionDescriptor,
+            device::{Device, DeviceDescriptor, DeviceRegistry},
+        },
+        stub::connection::{StubConnection, StubConnectionDescriptor, StubConnectionRegistry},
+    };
+
+    use super::Q30DeviceRegistry;
+
+    #[tokio::test]
+    async fn test_device_descriptors() {
+        let descriptor = StubConnectionDescriptor::new(
+            "Stub Device".to_string(),
+            "00:11:22:33:44:55".to_string(),
+        );
+        let device = Arc::new(StubConnection::new());
+        let devices = HashMap::from([(descriptor, device)]);
+        let connection_registry = StubConnectionRegistry::new(devices.to_owned());
+        let device_registry = Q30DeviceRegistry::new(connection_registry).await.unwrap();
+
+        let descriptors = device_registry.device_descriptors().await.unwrap();
+        let descriptor_values = descriptors
+            .iter()
+            .map(|descriptor| {
+                (
+                    descriptor.name().as_ref(),
+                    descriptor.mac_address().as_ref(),
+                )
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(
+            vec![("Stub Device", "00:11:22:33:44:55")],
+            descriptor_values,
+        );
+    }
+
+    #[tokio::test]
+    async fn test_get_device() {
+        let descriptor = StubConnectionDescriptor::new(
+            "Stub Device".to_string(),
+            "00:11:22:33:44:55".to_string(),
+        );
+        let device = Arc::new(StubConnection::new());
+        let (sender, receiver) = mpsc::channel(1);
+        sender
+            .send(vec![
+                0x09, 0xff, 0x00, 0x00, 0x01, 0x01, 0x01, 0x46, 0x00, 0x05, 0x00, 0x01, 0x00, 0x3c,
+                0xb4, 0x8f, 0xa0, 0x8e, 0xb4, 0x74, 0x88, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x02, 0x00, 0x01, 0x00, 0x30, 0x32, 0x2e, 0x33, 0x30, 0x33, 0x30, 0x32,
+                0x39, 0x30, 0x38, 0x36, 0x45, 0x43, 0x38, 0x32, 0x46, 0x31, 0x32, 0x41, 0x43, 0x30,
+            ])
+            .await
+            .unwrap();
+        device.set_inbound_packets_channel(Ok(receiver)).await;
+        device
+            .set_mac_address_return(Ok(descriptor.mac_address().to_owned()))
+            .await;
+        device
+            .set_name_return(Ok(descriptor.name().to_owned()))
+            .await;
+        for _ in 0..10 {
+            device.push_write_return(Ok(())).await;
+        }
+
+        let devices = HashMap::from([(descriptor, device)]);
+        let connection_registry = StubConnectionRegistry::new(devices.to_owned());
+        let device_registry = Q30DeviceRegistry::new(connection_registry).await.unwrap();
+
+        let device = device_registry
+            .device("00:11:22:33:44:55")
+            .await
+            .expect("must not fail to create device")
+            .expect("device must exist");
+
+        assert_eq!("00:11:22:33:44:55", device.mac_address().await.unwrap());
+    }
+
+    #[tokio::test]
+    async fn test_get_device_but_it_doesnt_exist() {
+        let descriptor = StubConnectionDescriptor::new(
+            "Stub Device".to_string(),
+            "00:11:22:33:44:55".to_string(),
+        );
+        let device = Arc::new(StubConnection::new());
+
+        let devices = HashMap::from([(descriptor, device)]);
+        let connection_registry = StubConnectionRegistry::new(devices.to_owned());
+        let device_registry = Q30DeviceRegistry::new(connection_registry).await.unwrap();
+
+        let maybe_device = device_registry
+            .device("00:00:22:33:44:55")
+            .await
+            .expect("must not fail to create device");
+
+        assert_eq!(true, maybe_device.is_none());
+    }
+}
