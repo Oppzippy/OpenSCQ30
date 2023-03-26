6acd5058b5dd02a415d264803850c2a2e2710400
Kyle Scheuing <kyle@kylescheuing.com>
2023-03-26 02:06:52 -0400

    build: separate locale scripts
    Updating po files shouldn't be done when building, only during
    development.

---
+++ scripts/build-locales.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+PROJECT_NAME="com.oppzippy.OpenSCQ30"
+
+script_path="$(readlink -f -- "$0")"
+script_dir="$(dirname -- $script_path)"
+cd "$script_dir/../gui"
+
+build_locale() {
+    mkdir -p "locales/$1/LC_MESSAGES"
+    msgfmt --output-file "locales/$1/LC_MESSAGES/$PROJECT_NAME.mo" "po/$1/$PROJECT_NAME.po"
+}
+
+existing_locales=$(find po -mindepth 1 -maxdepth 1 -type d | sed 's/^po\///')
+while read locale; do
+    echo "Building $locale"
+    build_locale "$locale"
+done <<< "$existing_locales"
--- scripts/update-po-files.sh
+++ scripts/update-po-files.sh
@@ -3,11 +3,11 @@ set -e
 set -o pipefail
 PROJECT_NAME="com.oppzippy.OpenSCQ30"
 
-return_dir=$(pwd)
 script_path="$(readlink -f -- "$0")"
 script_dir="$(dirname -- $script_path)"
 cd "$script_dir/../gui"
 
+echo "Generating template"
 if [[ -f "po/$PROJECT_NAME.pot" ]]; then
     xgettext --files-from po/POTFILES.in \
         --join-existing \
@@ -22,16 +22,13 @@ else
 fi
 
 update_locale() {
-    if [[ ! -d "po/$1" ]]; then
-        mkdir "po/$1"
-    fi
+    echo "Updating $1"
+    mkdir -p "po/$1"
     if [[ ! -f "po/$1/$PROJECT_NAME.po" ]]; then
         msginit --input "po/$PROJECT_NAME.pot" --locale "$1" --output "po/$1/$PROJECT_NAME.po"
     else
         msgmerge --update "po/$1/$PROJECT_NAME.po" "po/$PROJECT_NAME.pot"
     fi
-    mkdir -p "locales/$1/LC_MESSAGES"
-    msgfmt --output-file "locales/$1/LC_MESSAGES/$PROJECT_NAME.mo" "po/$1/$PROJECT_NAME.po"
 }
 
 while read line; do
@@ -39,5 +36,3 @@ while read line; do
         update_locale "$line"
     fi
 done <po/LINGUAS
-
-cd "$return_dir"
