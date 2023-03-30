Oppzippy/OpenSCQ30

commit 8e0454ac8f013550fc6c50df7729f5eff32b45dd

build: reorganize directories

packaging/flatpak/com.oppzippy.OpenSCQ30.yml
@@ -29,9 +29,9 @@ modules:
       - cargo --offline build --release --package openscq30_gui --verbose
       - ./scripts/build-locales.sh
       - mkdir -p /app/share/locale
-      - cp -r ./gui/locale/* /app/share/locale/
+      - cp -r ./gui/share/locale/* /app/share/locale/
       - install -Dm755 ./target/release/openscq30_gui -t /app/bin/
-      - install -Dm644 ./ci/flatpak/${FLATPAK_ID}.desktop -t /app/share/applications/
+      - install -Dm644 ./packaging/flatpak/${FLATPAK_ID}.desktop -t /app/share/applications/
       - install -Dm644 ./gui/resources/icon.svg /app/share/icons/hicolor/scalable/apps/${FLATPAK_ID}.svg
     sources:
       - type: dir

.github/workflows/build.yml
@@ -102,7 +102,7 @@ jobs:
           $env:Path = "C:\gtk-build\gtk\x64\release\bin;" + $env:Path
           $env:LIB = "C:\gtk-build\gtk\x64\release\lib;" + $env:LIB
           $env:INCLUDE = "C:\gtk-build\gtk\x64\release\include;C:\gtk-build\gtk\x64\release\include\cairo;C:\gtk-build\gtk\x64\release\include\glib-2.0;C:\gtk-build\gtk\x64\release\include\gobject-introspection-1.0;C:\gtk-build\gtk\x64\release\lib\glib-2.0\include;" + $env:INCLUDE
-          .\ci\windows\build.ps1
+          .\packaging\windows\build.ps1
       - name: Build cli
         run: cargo build --release --package openscq30_cli
 
@@ -110,7 +110,7 @@ jobs:
         uses: actions/upload-artifact@v3
         with:
           name: gui-windows
-          path: ci/windows/Output/OpenSCQ30_Setup.exe
+          path: packaging/windows/Output/OpenSCQ30_Setup.exe
       - name: Upload cli
         uses: actions/upload-artifact@v3
         with:

.gitmodules
@@ -1,3 +1,3 @@
-[submodule "ci/flatpak/flatpak-builder-tools"]
-	path = ci/flatpak/flatpak-builder-tools
+[submodule "packaging/flatpak/flatpak-builder-tools"]
+	path = packaging/flatpak/flatpak-builder-tools
 	url = https://github.com/flatpak/flatpak-builder-tools.git

gui/.gitignore
@@ -1,1 +1,1 @@
-/locale
+/share

gui/scripts/build-locales.sh
@@ -5,15 +5,18 @@ PROJECT_NAME="com.oppzippy.OpenSCQ30"
 
 script_path="$(readlink -f -- "$0")"
 script_dir="$(dirname -- $script_path)"
-cd "$script_dir/../gui"
+gui_dir="$script_dir/.."
+out_dir=$1
 
-build_locale() {
-    mkdir -p "locale/$1/LC_MESSAGES"
-    msgfmt --output-file "locale/$1/LC_MESSAGES/$PROJECT_NAME.mo" "po/$1/$PROJECT_NAME.po"
-}
+if [[ -z $out_dir ]]; then
+    out_dir="$gui_dir"
+fi
+
 
 existing_locales=$(find po -mindepth 1 -maxdepth 1 -type d | sed 's/^po\///')
+
 while read locale; do
     echo "Building $locale"
-    build_locale "$locale"
+    mkdir -p "$out_dir/share/locale/$locale/LC_MESSAGES"
+    msgfmt --output-file "$out_dir/share/locale/$locale/LC_MESSAGES/$PROJECT_NAME.mo" "$gui_dir/po/$locale/$PROJECT_NAME.po"
 done <<< "$existing_locales"

gui/scripts/update-po-files.sh
@@ -5,7 +5,7 @@ PROJECT_NAME="com.oppzippy.OpenSCQ30"
 
 script_path="$(readlink -f -- "$0")"
 script_dir="$(dirname -- $script_path)"
-cd "$script_dir/../gui"
+cd "$script_dir/.."
 
 echo "Generating template"
 if [[ -f "po/$PROJECT_NAME.pot" ]]; then

commit 627de1064a59cdb935154aa8d1df4b349d067bfa

build: fix flatpak
It was broken in 8e0454ac8f013550fc6c50df7729f5eff32b45dd

gui/scripts/build-locale.sh
@@ -13,7 +13,7 @@ if [[ -z $out_dir ]]; then
 fi
 
 
-existing_locales=$(find po -mindepth 1 -maxdepth 1 -type d | sed 's/^po\///')
+existing_locales=$(find "$gui_dir/po" -mindepth 1 -maxdepth 1 -type d -printf '%f\n')
 
 while read locale; do
     echo "Building $locale"

packaging/flatpak/com.oppzippy.OpenSCQ30.yml
@@ -27,9 +27,7 @@ modules:
     build-commands:
       - cargo --offline fetch --manifest-path Cargo.toml --verbose
       - cargo --offline build --release --package openscq30_gui --verbose
-      - ./scripts/build-locale.sh
-      - mkdir -p /app/share/locale
-      - cp -r ./gui/share/locale/* /app/share/locale/
+      - ./gui/scripts/build-locale.sh /app
       - install -Dm755 ./target/release/openscq30_gui -t /app/bin/
       - install -Dm644 ./packaging/flatpak/${FLATPAK_ID}.desktop -t /app/share/applications/
       - install -Dm644 ./gui/resources/icon.svg /app/share/icons/hicolor/scalable/apps/${FLATPAK_ID}.svg