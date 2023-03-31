e9c9afb38081a6a9b9ebf8fd0859133c2d967542
Kyle Scheuing <kyle@kylescheuing.com>
2023-03-30 20:51:20 -0400

    build: fix appimage

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -3,15 +3,17 @@ set -e
 set -o pipefail
 
 set_version_in_cargo_toml() {
-    # --null-data treats the entire file as one line. this mkes it easy to only replace the first occurrance of
+    # --null-data treats the entire file as one line. this makes it easy to only replace the first occurrance of
     # "version =" to avoid catching any dependency versions
     sed --in-place --regexp-extended --null-data "s/(\nversion *= *\")([0-9\.]+)(\"\n)/\1$2\3/" "$1"
 }
 
 set_version_in_build_gradle() {
-    # --null-data treats the entire file as one line. this mkes it easy to only replace the first occurrance of
-    # "version =" to avoid catching any dependency versions
     sed --in-place --regexp-extended --null-data "s/(\n *versionName \")([0-9\.]+)(\"\n)/\1$2\3/" "$1"
+}
+
+set_version_in_appimage_builder() {
+    sed --in-place --regexp-extended --null-data "s/(\n *version: *)([0-9\.]+)( *\n)/\1$2\3/" "$1"
 }
 
 if [ -z "$1" ]; then
@@ -31,3 +33,4 @@ set_version_in_cargo_toml cli/Cargo.toml "$1"
 set_version_in_cargo_toml gui/Cargo.toml "$1"
 set_version_in_cargo_toml lib/Cargo.toml "$1"
 set_version_in_build_gradle android/app/build.gradle "$1"
+set_version_in_appimage_builder packaging/appimage/AppImageBuilder.yml "$1"
