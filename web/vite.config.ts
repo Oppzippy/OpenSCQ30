5ac10a463ace1d3cd54ac0533ff33995c91ce3ae
Kyle Scheuing <kyle@kylescheuing.com>
2023-05-02 05:08:34 -0400

    feat(web): add PWA support

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,44 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import wasm from "vite-plugin-wasm";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import topLevelAwait from "vite-plugin-top-level-await";
+import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [wasm(), topLevelAwait(), react()],
+  plugins: [
+    wasm(),
+    topLevelAwait(),
+    react(),
+    VitePWA({
+      includeAssets: [
+        "favicon.svg",
+        "apple-touch-icon.png",
+        "masked-icon.svg",
+        "locales/**/*.json",
+      ],
+      manifest: {
+        name: "OpenSCQ30",
+        short_name: "OpenSCQ30",
+        description:
+          "Cross platform application for controlling settings of Soundcore Q30 headphones. Supports desktop (CLI and GTK4 GUI), Android, and Web Bluetooth.",
+        theme_color: "#474e3d",
+        scope: ".",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   assetsInclude: ["../web-wasm/pkg"],
   base: "./", // relative paths for github pages
 });
