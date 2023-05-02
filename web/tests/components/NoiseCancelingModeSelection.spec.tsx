98eaf9fc643faba0a62141936a91efc0a9a6d95b
Kyle Scheuing <kyle@kylescheuing.com>
2023-05-02 04:15:23 -0400

    test(web): replace usage of fireEvent with userEvent

--- web/tests/components/NoiseCancelingModeSelection.spec.tsx
+++ web/tests/components/NoiseCancelingModeSelection.spec.tsx
@@ -1,9 +1,16 @@
-import { fireEvent, render } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import { NoiseCancelingModeSelection } from "../../src/components/NoiseCancelingModeSelection";
 import { NoiseCancelingMode } from "../../wasm/pkg/openscq30_web_wasm";
 
 describe("NoiseCancelingModeSelection", () => {
+  let user: ReturnType<typeof userEvent.setup>;
+
+  beforeEach(() => {
+    user = userEvent.setup();
+  });
+
   it("should render", () => {
     const renderResult = render(
       <NoiseCancelingModeSelection
@@ -17,7 +24,7 @@ describe("NoiseCancelingModeSelection", () => {
     expect(renderResult.asFragment()).toMatchSnapshot();
   });
 
-  it("should fire onValueChanged when clicked", () => {
+  it("should fire onValueChanged when clicked", async () => {
     const setMode = vi.fn();
     const renderResult = render(
       <NoiseCancelingModeSelection
@@ -26,7 +33,7 @@ describe("NoiseCancelingModeSelection", () => {
       />
     );
 
-    fireEvent.click(renderResult.getByText("Outdoor"));
+    await user.click(renderResult.getByText("Outdoor"));
 
     expect(setMode).toHaveBeenCalledWith(NoiseCancelingMode.Outdoor);
   });
