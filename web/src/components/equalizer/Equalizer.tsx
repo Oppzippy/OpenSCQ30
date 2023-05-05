087678c6aad945138e72c47ad3b135c6f71b7580
Kyle Scheuing <kyle@kylescheuing.com>
2023-05-04 20:00:13 -0400

    feat(web): add Hz labels for eq bands

--- web/src/components/equalizer/Equalizer.tsx
+++ web/src/components/equalizer/Equalizer.tsx
@@ -1,4 +1,4 @@
-import { Divider, Stack } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import { VolumeSlider } from "./VolumeSlider";
 
 type Props = {
@@ -9,17 +9,31 @@ type Props = {
 
 export function Equalizer(props: Props) {
   return (
-    <Stack spacing={0.2} divider={<Divider />}>
+    <Grid2
+      container
+      rowSpacing={2}
+      columnSpacing={2}
+      sx={{
+        // 3 columns per row, so to have no bottom border on the last row, exclude the last three children
+        "& > :not(:nth-last-of-type(1), :nth-last-of-type(2), :nth-last-of-type(3))":
+          {
+            borderBottom: "1px solid",
+            borderColor: "divider",
+          },
+      }}
+    >
       {props.values.map((value, index) => {
         return (
           <VolumeSlider
             disabled={props.disabled}
             key={index}
+            // Starts at 100hz
+            hz={100 * Math.pow(2, index)}
             value={value}
             onValueChange={(newValue) => props.onValueChange(index, newValue)}
           />
         );
       })}
-    </Stack>
+    </Grid2>
   );
 }
--- web/src/components/equalizer/VolumeSlider.tsx
+++ web/src/components/equalizer/VolumeSlider.tsx
@@ -1,46 +1,68 @@
-import { Box, Input, Slider } from "@mui/material";
+import { Input, Slider, Typography } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2";
+import { useTranslation } from "react-i18next";
 
 type Props = {
+  hz: number;
   disabled?: boolean;
   value: number;
   onValueChange: (newValue: number) => void;
 };
 
 export function VolumeSlider(props: Props) {
+  const { t } = useTranslation();
   return (
-    <Box sx={{ display: "flex", columnGap: "2rem", alignItems: "start" }}>
-      <Slider
-        disabled={props.disabled}
-        value={props.value}
-        min={-12}
-        max={12}
-        step={0.1}
-        valueLabelDisplay="auto"
-        marks={[
-          { value: -12, label: "-12 dB" },
-          { value: 12, label: "12 dB" },
-        ]}
-        onChange={(_, value) => {
-          if (typeof value == "number") {
-            props.onValueChange(value);
-          } else {
-            throw Error(`Expected single number, got number array: ${value}`);
-          }
-        }}
-      />
-      <Input
-        disabled={props.disabled}
-        value={props.value}
-        onChange={(event) => props.onValueChange(Number(event.target.value))}
-        size="small"
-        inputProps={{
-          min: -0,
-          max: 12,
-          step: 0.1,
-          type: "number",
-          "aria-labelledby": "input-slider",
-        }}
-      />
-    </Box>
+    <>
+      {/* make sure Hz doesn't go on to a second line */}
+      <Grid2 xs={3} sm={2}>
+        <Typography>
+          {props.hz >= 10000
+            ? t("equalizer.khz", {
+                defaultValue: "{{ hz }} kHz",
+                replace: { hz: props.hz / 1000 },
+              })
+            : t("equalizer.hz", {
+                defaultValue: "{{ hz }} Hz",
+                replace: { hz: props.hz },
+              })}
+        </Typography>
+      </Grid2>
+      <Grid2 xs={7} sm={8}>
+        <Slider
+          disabled={props.disabled}
+          value={props.value}
+          min={-12}
+          max={12}
+          step={0.1}
+          valueLabelDisplay="auto"
+          marks={[
+            { value: -12, label: "-12 dB" },
+            { value: 12, label: "12 dB" },
+          ]}
+          onChange={(_, value) => {
+            if (typeof value == "number") {
+              props.onValueChange(value);
+            } else {
+              throw Error(`Expected single number, got number array: ${value}`);
+            }
+          }}
+        />
+      </Grid2>
+      <Grid2 xs={2}>
+        <Input
+          disabled={props.disabled}
+          value={props.value}
+          onChange={(event) => props.onValueChange(Number(event.target.value))}
+          size="small"
+          inputProps={{
+            min: -0,
+            max: 12,
+            step: 0.1,
+            type: "number",
+            "aria-labelledby": "input-slider",
+          }}
+        />
+      </Grid2>
+    </>
   );
 }
