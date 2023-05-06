e37c2f62011cf851f6c00b0bc92483194efaf4e6
Kyle Scheuing <kyle@kylescheuing.com>
2023-05-06 15:27:38 -0400

    fix(web): add roboto font
    Material UI expects that font to be present:
    https://mui.com/material-ui/getting-started/installation/

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App.tsx";
 import { CssBaseline } from "@mui/material";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "./i18n.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
