14ae8dcf7e561ddf49c410c84865c7c4ca3e1029
Kyle Scheuing <kyle@kylescheuing.com>
2023-05-15 18:25:38 -0400

    fix(android): fix EqualizerLine height
    The font size in this context is actually bodyLarge, not bodyMedium.
    Rather than hardcoding either of these, though, LocalTextStyle.current
    can be used.

--- android/app/src/main/java/com/oppzippy/openscq30/features/ui/equalizer/composables/ProfileSelectionRow.kt
+++ android/app/src/main/java/com/oppzippy/openscq30/features/ui/equalizer/composables/ProfileSelectionRow.kt
@@ -3,25 +3,26 @@ package com.oppzippy.openscq30.features.ui.equalizer.composables
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.oppzippy.openscq30.ui.theme.OpenSCQ30Theme
 
 @Composable
 fun ProfileSelectionRow(name: String, volumeAdjustments: List<Byte>?) {
     Row(
-        modifier = Modifier
-            .fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Text(name)
         if (volumeAdjustments != null) {
-            val lineHeightSp = MaterialTheme.typography.bodyMedium.fontSize
+            val lineHeightSp = LocalTextStyle.current.fontSize
             val lineHeightDp = with(LocalDensity.current) {
                 lineHeightSp.toDp()
             }
@@ -31,5 +32,16 @@ fun ProfileSelectionRow(name: String, volumeAdjustments: List<Byte>?) {
                 height = lineHeightDp,
             )
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DefaultPreview() {
+    OpenSCQ30Theme {
+        ProfileSelectionRow(
+            name = "Test Profile",
+            volumeAdjustments = listOf(0, 10, 50, 100, -100, -50, -10, 0),
+        )
     }
 }
