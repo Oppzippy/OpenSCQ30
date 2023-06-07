f7befd8753c75a931b5e6a91dc3d13962a827d8b
Kyle Scheuing <kyle@kylescheuing.com>
2023-06-06 21:30:05 -0400

    build: remove flatpak
    e85609f7bb954d27d361ac1e631bb3cc16be7426 was a mistake and did not
    delete the flatpak directory.

--- packaging/flatpak/install-flatpak.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./flatpak-builder-tools/cargo/flatpak-cargo-generator.py ../../Cargo.lock -o generated-sources.json
-flatpak-builder --install repo com.oppzippy.OpenSCQ30.yml --user --force-clean
