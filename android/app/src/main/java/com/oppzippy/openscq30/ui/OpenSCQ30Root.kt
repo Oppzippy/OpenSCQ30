56f1463bdd01fc0470b23a9a984c03b5ca417991
Kyle Scheuing <kyle@kylescheuing.com>
2023-06-30 20:21:07 -0400

    fix(android): wrap sound mode/eq changes in try/catch
    A toast will be shown if an exception is thrown rather than crashing
    the app.

--- android/app/src/main/java/com/oppzippy/openscq30/ui/OpenSCQ30Root.kt
+++ android/app/src/main/java/com/oppzippy/openscq30/ui/OpenSCQ30Root.kt
@@ -1,28 +1,32 @@
 package com.oppzippy.openscq30.ui
 
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.compose.BackHandler
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.Crossfade
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
+import com.oppzippy.openscq30.R
 import com.oppzippy.openscq30.ui.deviceselection.DeviceSelectionRoot
 import com.oppzippy.openscq30.ui.devicesettings.composables.DeviceSettingsRoot
 import com.oppzippy.openscq30.ui.devicesettings.models.UiDeviceState
 import com.oppzippy.openscq30.ui.theme.OpenSCQ30Theme
-import kotlinx.coroutines.flow.asStateFlow
 
 @Composable
 fun OpenSCQ30Root(viewModel: RootViewModel = hiltViewModel()) {
     val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
 
     OpenSCQ30Theme {
-        DisposableEffect(1) {
+        DisposableEffect(viewModel) {
             val observer = LifecycleEventObserver { _, event ->
                 when (event) {
                     Lifecycle.Event.ON_START -> viewModel.bind()
@@ -44,9 +48,21 @@ fun OpenSCQ30Root(viewModel: RootViewModel = hiltViewModel()) {
                 DeviceSettingsRoot(
                     deviceState = deviceState,
                     onBack = { viewModel.deselectDevice() },
-                    onAmbientSoundModeChange = { viewModel.setAmbientSoundMode(it) },
-                    onNoiseCancelingModeChange = { viewModel.setNoiseCancelingMode(it) },
-                    onEqualizerConfigurationChange = { viewModel.setEqualizerConfiguration(it) },
+                    onAmbientSoundModeChange = {
+                        withErrorToast(context) {
+                            viewModel.setAmbientSoundMode(it)
+                        }
+                    },
+                    onNoiseCancelingModeChange = {
+                        withErrorToast(context) {
+                            viewModel.setNoiseCancelingMode(it)
+                        }
+                    },
+                    onEqualizerConfigurationChange = {
+                        withErrorToast(context) {
+                            viewModel.setEqualizerConfiguration(it)
+                        }
+                    },
                 )
             } else {
                 val devices by viewModel.devices.collectAsState()
@@ -57,5 +73,16 @@ fun OpenSCQ30Root(viewModel: RootViewModel = hiltViewModel()) {
                 )
             }
         }
+    }
+}
+
+private fun withErrorToast(context: Context, f: () -> Unit) {
+    try {
+        f()
+    } catch (ex: Exception) {
+        Log.e("OpenSCQ30Root", "", ex)
+        Toast.makeText(
+            context, R.string.an_error_has_occurred, Toast.LENGTH_SHORT,
+        ).show()
     }
 }
