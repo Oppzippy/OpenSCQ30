869c7f6a356e387e30b074bae9b19499ecf23b27
Kyle Scheuing <kyle@kylescheuing.com>
2023-07-03 02:42:22 -0400

    test(android): add test for bluetooth permission check
    Also fixes tests broken by adding
    a contructor parameter in 5ff96d9092e1a8a8c4e6bb03624bdd91313579fe

--- android/app/src/test/java/com/oppzippy/openscq30/ui/deviceselection/DeviceSelectionViewModelTest.kt
+++ android/app/src/test/java/com/oppzippy/openscq30/ui/deviceselection/DeviceSelectionViewModelTest.kt
@@ -1,10 +1,13 @@
 package com.oppzippy.openscq30.ui.deviceselection
 
+import android.app.Application
+import android.content.pm.PackageManager
 import com.oppzippy.openscq30.features.bluetoothdeviceprovider.BluetoothDevice
 import com.oppzippy.openscq30.features.bluetoothdeviceprovider.BluetoothDeviceProvider
 import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit4.MockKRule
+import io.mockk.mockk
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
@@ -14,21 +17,26 @@ class DeviceSelectionViewModelTest {
     val mockkRule = MockKRule(this)
 
     @RelaxedMockK
+    lateinit var application: Application
+
+    @RelaxedMockK
     lateinit var deviceProvider: BluetoothDeviceProvider
 
     @Test
     fun hasInitialDevices() {
         val devices = listOf(BluetoothDevice("Test Device", "00:00:00:00:00:00"))
         every { deviceProvider.getDevices() } returns devices
+        every { application.checkSelfPermission(any()) } returns PackageManager.PERMISSION_GRANTED
 
-        val viewModel = DeviceSelectionViewModel(deviceProvider)
+        val viewModel = DeviceSelectionViewModel(application, deviceProvider)
         assertEquals(devices, viewModel.devices.value)
     }
 
     @Test
     fun doesNotUpdateDevicesUntilRefreshDevicesIsCalled() {
+        every { application.checkSelfPermission(any()) } returns PackageManager.PERMISSION_GRANTED
         every { deviceProvider.getDevices() } returns emptyList()
-        val viewModel = DeviceSelectionViewModel(deviceProvider)
+        val viewModel = DeviceSelectionViewModel(application, deviceProvider)
 
         val devices = listOf(BluetoothDevice("Test Device", "00:00:00:00:00:00"))
         every { deviceProvider.getDevices() } returns devices
@@ -36,5 +44,15 @@ class DeviceSelectionViewModelTest {
         assertEquals(emptyList<BluetoothDevice>(), viewModel.devices.value)
         viewModel.refreshDevices()
         assertEquals(devices, viewModel.devices.value)
+    }
+
+    @Test
+    fun hasNoDevicesWithoutPermission() {
+        val devices = listOf(BluetoothDevice("Test Device", "00:00:00:00:00:00"))
+        every { deviceProvider.getDevices() } returns devices
+        every { application.checkSelfPermission(any()) } returns PackageManager.PERMISSION_DENIED
+
+        val viewModel = DeviceSelectionViewModel(application, deviceProvider)
+        assertEquals(emptyList<BluetoothDevice>(), viewModel.devices.value)
     }
 }
