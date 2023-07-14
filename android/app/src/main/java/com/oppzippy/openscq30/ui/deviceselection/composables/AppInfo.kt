8b954f023b796563e9be5f02ac871671ed61b7f1
Kyle Scheuing <kyle@kylescheuing.com>
2023-07-14 00:32:29 -0400

    fix(android): info screen text color poor readability
    The text color wasn't being inherited from the theme, only the size was.
    Hyperlink color is still not handled, but the default color is fine with
    both light and dark themes.

--- android/app/src/main/java/com/oppzippy/openscq30/ui/deviceselection/composables/AppInfo.kt
+++ android/app/src/main/java/com/oppzippy/openscq30/ui/deviceselection/composables/AppInfo.kt
@@ -14,7 +14,10 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -22,6 +25,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.colorspace.ColorSpaces
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -59,7 +63,9 @@ fun AppInfo(onBackClick: () -> Unit) {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.fillMaxHeight(),
             ) {
-                HtmlText(stringResource(R.string.source_code))
+                ProvideTextStyle(MaterialTheme.typography.bodyLarge) {
+                    HtmlText(stringResource(R.string.source_code))
+                }
             }
         }
     })
@@ -69,9 +75,17 @@ fun AppInfo(onBackClick: () -> Unit) {
 private fun HtmlText(text: String, modifier: Modifier = Modifier) {
     val context = LocalContext.current
     val customLinkifyTextView = remember { TextView(context) }
-    val font = MaterialTheme.typography.bodyLarge
+
+    val font = LocalTextStyle.current
+    val color = LocalContentColor.current.convert(ColorSpaces.Srgb)
+    val alpha = (color.alpha * 255).toInt()
+    val red = (color.red * 255).toInt()
+    val green = (color.green * 255).toInt()
+    val blue = (color.blue * 255).toInt()
+
     AndroidView(modifier = modifier, factory = { customLinkifyTextView }) { textView ->
         textView.textSize = font.fontSize.value
+        textView.setTextColor((alpha shl 24) or (red shl 16) or (green shl 8) or blue)
         val html = Html.fromHtml(text, Html.FROM_HTML_MODE_COMPACT)
         textView.text = html
         textView.movementMethod = LinkMovementMethod.getInstance()
