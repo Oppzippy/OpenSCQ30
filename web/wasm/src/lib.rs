d510dece471c3a5ab94f7c985d2dc2d399934a3f
Kyle Scheuing <kyle@kylescheuing.com>
2023-08-07 05:57:57 -0400

    fix(web): write tracing logs to console
    They were going nowhere previously.

--- web/wasm/src/lib.rs
+++ web/wasm/src/lib.rs
@@ -6,3 +6,11 @@ pub mod web_bluetooth_connection;
 pub use device::*;
 pub use equalizer_helper::*;
 pub use soundcore_device_utils::*;
+
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[wasm_bindgen(start)]
+pub fn initialize() {
+    console_error_panic_hook::set_once();
+    tracing_wasm::set_as_global_default();
+}
