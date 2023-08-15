2c46155e3b9a93c35934aaa9b3c5ac87034c6dc6
Kyle Scheuing <kyle@kylescheuing.com>
2023-08-14 22:57:40 -0400

    feat(lib): add request battery level/charging packets

--- lib/src/packets/outbound.rs
+++ lib/src/packets/outbound.rs
@@ -1,4 +1,6 @@
 mod outbound_packet;
+mod request_battery_charging_packet;
+mod request_battery_level_packet;
 mod request_firmware_version_packet;
 mod request_state_packet;
 mod set_custom_button_model_packet;
@@ -7,6 +9,8 @@ mod set_equalizer_with_drc;
 mod set_sound_mode;
 
 pub use outbound_packet::*;
+pub use request_battery_charging_packet::*;
+pub use request_battery_level_packet::*;
 pub use request_firmware_version_packet::*;
 pub use request_state_packet::*;
 pub use set_custom_button_model_packet::*;
---
+++ lib/src/packets/outbound/request_battery_charging_packet.rs
@@ -0,0 +1,31 @@
+use super::OutboundPacket;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
+pub struct RequestBatteryChargingPacket {}
+
+impl RequestBatteryChargingPacket {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl OutboundPacket for RequestBatteryChargingPacket {
+    fn command(&self) -> [u8; 7] {
+        [0x08, 0xee, 0x00, 0x00, 0x00, 0x01, 0x04]
+    }
+
+    fn body(&self) -> Vec<u8> {
+        Vec::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::packets::outbound::{OutboundPacketBytes, RequestBatteryChargingPacket};
+
+    #[test]
+    fn it_matches_a_manually_crafted_packet() {
+        let expected: &[u8] = &[0x08, 0xee, 0x00, 0x00, 0x00, 0x01, 0x04, 0x0a, 0x00, 0x05];
+        assert_eq!(expected, RequestBatteryChargingPacket::new().bytes())
+    }
+}
