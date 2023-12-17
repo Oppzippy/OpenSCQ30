Oppzippy/OpenSCQ30

commit 33a2076bda2612addf1c18726125c4422a8b51e5

refactor(android): use SoundcoreDevice from lib
Rather than reimplementing SoundcoreDevice again in kotlin, a simple
adapter ManualConnection is used to pass bluetooth packets to/from rust.
To simplify complex native objects, protobuf is now used for passing
state between kotiln and rust.

Due to the simplified object tree, freeing objects manually in kotlin
is now practical, so flapigen is swapped for uniffi. flapigen supported
auto freeing objects when they get garbage collected, but uniffi does
not, which is fine now.

android/app/src/main/java/com/oppzippy/openscq30/features/soundcoredevice/impl/SoundcoreDeviceCallbackHandler.kt
@@ -13,12 +13,15 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.util.Log
-import com.oppzippy.openscq30.lib.bindings.SoundcoreDeviceUtils
-import kotlinx.coroutines.channels.BufferOverflow
-import kotlinx.coroutines.flow.MutableSharedFlow
+import com.oppzippy.openscq30.lib.bindings.ConnectionWriter
+import com.oppzippy.openscq30.lib.bindings.ManualConnection
+import com.oppzippy.openscq30.lib.bindings.isSoundcoreServiceUuid
+import com.oppzippy.openscq30.lib.bindings.readCharacteristicUuid
+import com.oppzippy.openscq30.lib.bindings.writeCharacteristicUuid
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import java.util.UUID
@@ -26,19 +29,30 @@ import java.util.concurrent.ConcurrentLinkedQueue
 
 @SuppressLint("MissingPermission")
 @Suppress("DEPRECATION")
-class SoundcoreDeviceCallbackHandler(context: Context) : BluetoothGattCallback() {
+class SoundcoreDeviceCallbackHandler(
+    context: Context,
+    private val coroutineScope: CoroutineScope,
+) :
+    BluetoothGattCallback(), ConnectionWriter, AutoCloseable {
+    // TODO make not lateinit
     private lateinit var gatt: BluetoothGatt
     private var readCharacteristic: BluetoothGattCharacteristic? = null
     private var writeCharacteristic: BluetoothGattCharacteristic? = null
     private var commandQueue: ConcurrentLinkedQueue<Command> = ConcurrentLinkedQueue()
     private var isLocked: Boolean = false
-    private val _packetsFlow: MutableSharedFlow<Packet> =
-        MutableSharedFlow(0, 50, BufferOverflow.DROP_OLDEST)
-    val packetsFlow = _packetsFlow.asSharedFlow()
     private val _isDisconnected = MutableStateFlow(false)
+
+    // TODO handle disconnects
     val isDisconnected = _isDisconnected.asStateFlow()
     private var _serviceUuid = MutableStateFlow<UUID?>(null)
     val serviceUuid = _serviceUuid.asStateFlow()
+    private var manualConnection: ManualConnection? = null
+
+    @Synchronized
+    fun setManualConnection(manualConnection: ManualConnection) {
+        this.manualConnection?.close()
+        this.manualConnection = manualConnection
+    }
 
     val adapter: BluetoothAdapter = context.getSystemService(BluetoothManager::class.java).adapter
     private val broadcastReceiver = object : BroadcastReceiver() {
@@ -57,6 +71,19 @@ class SoundcoreDeviceCallbackHandler(context: Context) : BluetoothGattCallback()
             broadcastReceiver,
             IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED),
         )
+    }
+
+    @Synchronized
+    override fun close() {
+        manualConnection?.close()
+    }
+
+    override fun writeWithResponse(data: ByteArray) {
+        queueCommand(Command.Write(data))
+    }
+
+    override fun writeWithoutResponse(data: ByteArray) {
+        queueCommand(Command.Write(data))
     }
 
     @Synchronized
@@ -129,6 +156,7 @@ class SoundcoreDeviceCallbackHandler(context: Context) : BluetoothGattCallback()
         next()
     }
 
+    @Deprecated("Deprecated in Java")
     @Synchronized
     override fun onCharacteristicChanged(
         _gatt: BluetoothGatt?,
@@ -136,13 +164,8 @@ class SoundcoreDeviceCallbackHandler(context: Context) : BluetoothGattCallback()
     ) {
         if (characteristic != null) {
             val bytes = characteristic.value
-            val packet = Packet.fromBytes(bytes)
-            if (packet != null) {
-                if (!_packetsFlow.tryEmit(packet)) {
-                    Log.e("SoundcoreDeviceCallbacks", "failed to emit packet to flow")
-                }
-            } else {
-                Log.i("unknown-packet", "got unknown packet: $bytes")
+            coroutineScope.launch {
+                manualConnection?.addInboundPacket(bytes)
             }
             isLocked = false
             next()
@@ -159,6 +182,7 @@ class SoundcoreDeviceCallbackHandler(context: Context) : BluetoothGattCallback()
         next()
     }
 
+    @Deprecated("Deprecated in Java")
     @Synchronized
     override fun onDescriptorRead(
         _gatt: BluetoothGatt?,
@@ -180,17 +204,14 @@ class SoundcoreDeviceCallbackHandler(context: Context) : BluetoothGattCallback()
     @Synchronized
     override fun onServicesDiscovered(_gatt: BluetoothGatt?, status: Int) {
         val service = gatt.services.first {
-            SoundcoreDeviceUtils.isSoundcoreServiceUuid(
-                it.uuid.mostSignificantBits,
-                it.uuid.leastSignificantBits,
-            )
+            isSoundcoreServiceUuid(it.uuid)
         }
         _serviceUuid.value = service.uuid
 
         writeCharacteristic =
-            service.getCharacteristic(UUID.fromString(SoundcoreDeviceUtils.writeCharacteristicUuid()))
+            service.getCharacteristic(UUID.fromString(writeCharacteristicUuid()))
         val readCharacteristic =
-            service.getCharacteristic(UUID.fromString(SoundcoreDeviceUtils.readCharacteristicUuid()))
+            service.getCharacteristic(UUID.fromString(readCharacteristicUuid()))
         this.readCharacteristic = readCharacteristic
 
         gatt.setCharacteristicNotification(readCharacteristic, true)

android/app/src/main/java/com/oppzippy/openscq30/features/soundcoredevice/impl/SoundcoreDeviceConnectorImpl.kt
@@ -3,32 +3,27 @@ package com.oppzippy.openscq30.features.soundcoredevice.impl
 import android.bluetooth.BluetoothDevice
 import android.content.Context
 import android.util.Log
-import com.oppzippy.openscq30.features.soundcoredevice.api.SoundcoreDevice
 import com.oppzippy.openscq30.features.soundcoredevice.api.SoundcoreDeviceConnector
-import com.oppzippy.openscq30.lib.bindings.RequestFirmwareVersionPacket
-import com.oppzippy.openscq30.lib.bindings.RequestStatePacket
-import com.oppzippy.openscq30.lib.wrapper.toSoundcoreDeviceState
+import com.oppzippy.openscq30.lib.bindings.ManualConnection
+import com.oppzippy.openscq30.lib.bindings.newSoundcoreDevice
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.TimeoutCancellationException
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withTimeout
-import kotlinx.coroutines.withTimeoutOrNull
 import java.util.concurrent.TimeoutException
 import kotlin.time.Duration.Companion.seconds
 
 class SoundcoreDeviceConnectorImpl(
     private val context: Context,
     private val deviceFinder: BluetoothDeviceFinder,
-    private val callbackHandlerFactory: SoundcoreDeviceCallbackHandlerFactory,
-    private val deviceFactory: SoundcoreDeviceFactory,
 ) : SoundcoreDeviceConnector {
     @Throws(SecurityException::class)
     override suspend fun connectToSoundcoreDevice(
         macAddress: String,
-        scope: CoroutineScope,
+        coroutineScope: CoroutineScope,
     ): SoundcoreDevice? {
         val bluetoothDevice = deviceFinder.findByMacAddress(macAddress) ?: return null
-        val callbacks = callbackHandlerFactory.createSoundcoreDeviceCallbackHandler(context)
+        val callbacks =
+            SoundcoreDeviceCallbackHandler(context = context, coroutineScope = coroutineScope)
         val gatt =
             bluetoothDevice.connectGatt(context, false, callbacks, BluetoothDevice.TRANSPORT_LE)
         if (!gatt.connect()) {
@@ -44,28 +39,27 @@ class SoundcoreDeviceConnectorImpl(
             throw TimeoutException("Timeout waiting for GATT services").initCause(ex)
         }
 
-        var packet: Packet.StateUpdate? = null
-        for (i in 0..3) {
-            callbacks.queueCommand(Command.Write(RequestStatePacket().bytes()))
-            packet = withTimeoutOrNull(1.seconds) {
-                callbacks.packetsFlow.first { it is Packet.StateUpdate } as Packet.StateUpdate
-            }
-            if (packet != null) {
-                break
-            }
-        }
-
-        if (packet == null) {
-            throw TimeoutException("Timeout waiting for initial device state")
-        }
-
-        callbacks.queueCommand(Command.Write(RequestFirmwareVersionPacket().bytes()))
-
-        return deviceFactory.createSoundcoreDevice(
-            gatt,
-            callbacks,
-            scope,
-            packet.inner.toSoundcoreDeviceState(),
+        val serviceUuid = callbacks.serviceUuid.value ?: return null
+        val connection = ManualConnection(
+            name = bluetoothDevice.name,
+            macAddress = bluetoothDevice.address,
+            serviceUuid = serviceUuid,
+            connectionWriter = callbacks,
+        )
+        callbacks.setManualConnection(connection)
+        val nativeDevice = newSoundcoreDevice(connection)
+        return SoundcoreDevice(
+            name = nativeDevice.name(),
+            macAddress = nativeDevice.macAddress(),
+            bleServiceUuid = nativeDevice.serviceUuid(),
+            cleanUp = {
+                callbacks.close()
+                gatt.disconnect()
+                gatt.close()
+            },
+            nativeDevice = nativeDevice,
+            coroutineScope = coroutineScope,
+            initialState = nativeDevice.state(),
         )
     }
 }

commit 8bfc847b789f117dc912197a45f5c5bf78635b08

fix(android): handle disconnects
This was a regression introduced in 33a2076bda2612addf1c18726125c4422a8b51e5

android/app/src/main/java/com/oppzippy/openscq30/features/soundcoredevice/impl/SoundcoreDeviceCallbackHandler.kt
@@ -41,8 +41,6 @@ class SoundcoreDeviceCallbackHandler(
     private var commandQueue: ConcurrentLinkedQueue<Command> = ConcurrentLinkedQueue()
     private var isLocked: Boolean = false
     private val _isDisconnected = MutableStateFlow(false)
-
-    // TODO handle disconnects
     val isDisconnected = _isDisconnected.asStateFlow()
     private var _serviceUuid = MutableStateFlow<UUID?>(null)
     val serviceUuid = _serviceUuid.asStateFlow()

android/app/src/main/java/com/oppzippy/openscq30/features/soundcoredevice/impl/SoundcoreDeviceConnectorImpl.kt
@@ -8,6 +8,7 @@ import com.oppzippy.openscq30.lib.bindings.ManualConnection
 import com.oppzippy.openscq30.lib.bindings.newSoundcoreDevice
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withTimeout
 import java.util.concurrent.TimeoutException
 import kotlin.time.Duration.Companion.seconds
@@ -47,8 +48,9 @@ class SoundcoreDeviceConnectorImpl(
             connectionWriter = callbacks,
         )
         callbacks.setManualConnection(connection)
+
         val nativeDevice = newSoundcoreDevice(connection)
-        return SoundcoreDevice(
+        val soundcoreDevice = SoundcoreDevice(
             name = nativeDevice.name(),
             macAddress = nativeDevice.macAddress(),
             bleServiceUuid = nativeDevice.serviceUuid(),
@@ -61,5 +63,18 @@ class SoundcoreDeviceConnectorImpl(
             coroutineScope = coroutineScope,
             initialState = nativeDevice.state(),
         )
+
+        // SoundcoreDevice and SoundcoreDeviceCallbackHandler are intentionally unaware of each other,
+        // so connecting isDisconnected to SoundcoreDevice's close must be done outside of either of
+        // the two classes.
+        coroutineScope.launch {
+            callbacks.isDisconnected.collect { isDisconnected ->
+                if (isDisconnected) {
+                    soundcoreDevice.close()
+                }
+            }
+        }
+
+        return soundcoreDevice
     }
 }