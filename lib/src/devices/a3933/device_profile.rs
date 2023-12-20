d8fddc1be33d09ee109ed2d1da9b5d5c02afc982
Kyle Scheuing <kyle@kylescheuing.com>
2023-12-20 15:19:07 -0500

    fix(lib): a3933 EQ band 9 and 10 handling
    Bands 9 and 10 should be ignored, just like the A3945.

--- lib/src/devices/a3933/device_profile.rs
+++ lib/src/devices/a3933/device_profile.rs
@@ -1,5 +1,10 @@
-use crate::device_profiles::{
-    DeviceProfile, NoiseCancelingModeType, SoundModeProfile, TransparencyModeType,
+use std::sync::Arc;
+
+use crate::{
+    device_profiles::{
+        DeviceProfile, NoiseCancelingModeType, SoundModeProfile, TransparencyModeType,
+    },
+    devices::a3945::device_profile::A3945Dispatcher,
 };
 
 pub const A3933_DEVICE_PROFILE: DeviceProfile = DeviceProfile {
@@ -9,12 +14,13 @@ pub const A3933_DEVICE_PROFILE: DeviceProfile = DeviceProfile {
     }),
     has_hear_id: false,
     num_equalizer_channels: 2,
-    num_equalizer_bands: 10,
+    num_equalizer_bands: 8,
     has_dynamic_range_compression: true,
     dynamic_range_compression_min_firmware_version: None,
     has_custom_button_model: true,
     has_wear_detection: false,
     has_touch_tone: false,
     has_auto_power_off: false,
-    custom_dispatchers: None,
+    // The A3933 has the same quirks as the A3945
+    custom_dispatchers: Some(|| Arc::new(A3945Dispatcher::default())),
 };
