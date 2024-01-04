0b6d8b735f9e79dd9ca04c1245a10ba238c58215
Kyle Scheuing <kyle@kylescheuing.com>
2024-01-04 16:42:43 -0500

    build(android): update AGP to 8.2.1

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,8 +2,8 @@ import org.jlleitschuh.gradle.ktlint.KtlintExtension
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("com.android.library") version "8.2.0" apply false
+    id("com.android.application") version "8.2.1" apply false
+    id("com.android.library") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
     id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
