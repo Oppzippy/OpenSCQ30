3f0c5cd3e6f25bc60253e0045c2c05eff785c2fc
Kyle Scheuing <kyle@kylescheuing.com>
2024-01-15 18:57:21 -0500

    build: v1.10.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "com.oppzippy.openscq30"
         minSdk = 26
         targetSdk = 34
-        versionCode = 19
-        versionName = "1.10.3"
+        versionCode = 20
+        versionName = "1.10.4"
 
         testInstrumentationRunner = "com.oppzippy.openscq30.HiltTestRunner"
         vectorDrawables {
