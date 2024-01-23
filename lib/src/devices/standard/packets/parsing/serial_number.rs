c98b9b0f55c7b9b2a6e103d7406ff03d5cca743d
Kyle Scheuing <kyle@kylescheuing.com>
2024-01-23 17:29:22 -0500

    fix(lib): validate serial numbers
    This currently has no effect, but it will likely be helpful as the
    number of supported devices goes up for reducing the likelihood of a
    state update packet being successfully parseable by two different state
    update packet parsers.

--- lib/src/devices/standard/packets/parsing/serial_number.rs
+++ lib/src/devices/standard/packets/parsing/serial_number.rs
@@ -1,5 +1,5 @@
 use nom::{
-    combinator::map,
+    combinator::map_opt,
     error::{context, ContextError, ParseError},
 };
 
@@ -10,5 +10,18 @@ use super::{take_str, ParseResult};
 pub fn take_serial_number<'a, E: ParseError<&'a [u8]> + ContextError<&'a [u8]>>(
     input: &'a [u8],
 ) -> ParseResult<SerialNumber, E> {
-    context("serial number", map(take_str(16usize), SerialNumber::from))(input)
+    context(
+        "serial number",
+        map_opt(take_str(16usize), |s| {
+            // Serial number is 4 digit model number followed by mac address with pairs in reverse order.
+            // ie. device model 1234 with max address 55:66:77:88:99:AA would be 1234AA9988776655
+            // The mac address is hex, and the model number is base 10 digits only, so we can use that
+            // to try to avoid parsing things that aren't serial numbers as one.
+            if s.chars().all(|c| c.is_ascii_hexdigit()) {
+                Some(SerialNumber::from(s))
+            } else {
+                None
+            }
+        }),
+    )(input)
 }
