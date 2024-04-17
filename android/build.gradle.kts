b9e8b9dcc54ea4004179cd68383336d920b9c4f4
Kyle Scheuing <kyle@kylescheuing.com>
2024-04-16 21:30:54 -0400

    build: update dependencies

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,8 +2,8 @@ import org.jlleitschuh.gradle.ktlint.KtlintExtension
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.1" apply false
-    id("com.android.library") version "8.3.1" apply false
+    id("com.android.application") version "8.3.2" apply false
+    id("com.android.library") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
