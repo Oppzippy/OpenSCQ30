4193cc0723a44868b367a7dadd290c4dc218cfea
Kyle Scheuing <kyle@kylescheuing.com>
2024-05-15 07:08:23 -0400

    build(gui): remove build-locale output dir arg
    Rather than outputting to a different location, the output should be
    copied from the default output location (gui/share/)

--- gui/scripts/build-locale.sh
+++ gui/scripts/build-locale.sh
@@ -5,22 +5,16 @@ PROJECT_NAME="com.oppzippy.OpenSCQ30"
 script_path="$(readlink -f -- "$0")"
 script_dir="$(dirname -- "$script_path")"
 gui_dir="$script_dir/.."
-out_dir=$1
-
-if [[ -z $out_dir ]]; then
-    out_dir="$gui_dir"
-fi
-
 
 existing_locales=$(find "$gui_dir/po" -mindepth 1 -maxdepth 1 -type d -printf '%f\n')
 
 # Default locale is en
 echo "Building en"
-mkdir -p "$out_dir/share/locale/en/LC_MESSAGES"
-msgfmt --output-file "$out_dir/share/locale/en/LC_MESSAGES/$PROJECT_NAME.mo" "$gui_dir/po/$PROJECT_NAME.pot"
+mkdir -p "$gui_dir/share/locale/en/LC_MESSAGES"
+msgfmt --output-file "$gui_dir/share/locale/en/LC_MESSAGES/$PROJECT_NAME.mo" "$gui_dir/po/$PROJECT_NAME.pot"
 
 while read -r locale; do
     echo "Building $locale"
-    mkdir -p "$out_dir/share/locale/$locale/LC_MESSAGES"
-    msgfmt --output-file "$out_dir/share/locale/$locale/LC_MESSAGES/$PROJECT_NAME.mo" "$gui_dir/po/$locale/$PROJECT_NAME.po"
+    mkdir -p "$gui_dir/share/locale/$locale/LC_MESSAGES"
+    msgfmt --output-file "$gui_dir/share/locale/$locale/LC_MESSAGES/$PROJECT_NAME.mo" "$gui_dir/po/$locale/$PROJECT_NAME.po"
 done <<< "$existing_locales"
