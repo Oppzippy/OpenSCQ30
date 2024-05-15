2015ff3da4a51d9ec6d1adb02b1b6aa132b58b0d
Kyle Scheuing <kyle@kylescheuing.com>
2024-05-15 07:13:15 -0400

    build(gui): fix locale installation

--- gui/scripts/install.sh
+++ gui/scripts/install.sh
@@ -17,4 +17,4 @@ install -Dm755 "../target/release/openscq30_gui$bin_ext" -t "$install_prefix/bin
 install -Dm644 ./resources/com.oppzippy.OpenSCQ30.desktop -t "$install_prefix/share/applications"
 install -Dm644 ./resources/com.oppzippy.OpenSCQ30.metainfo.xml -t "$install_prefix/share/metainfo"
 install -Dm644 ./resources/com.oppzippy.OpenSCQ30.svg -t "$install_prefix/share/icons/hicolor/scalable/apps"
-cp -r ./share/* "$install_prefix/"
+cp -r ./share/* "$install_prefix/share/"
