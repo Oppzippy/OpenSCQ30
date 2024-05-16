6a13ad9299da8d68bbc8db210fa38f7a65bab8ba
Kyle Scheuing <kyle@kylescheuing.com>
2024-05-16 07:37:42 -0400

    build(cli): fix install script unassigned variable

--- cli/scripts/install.sh
+++ cli/scripts/install.sh
@@ -14,7 +14,7 @@ esac
 echo Installing binary
 install -Dm755 "../target/release/openscq30_cli$bin_ext" -t "$install_prefix/bin/"
 
-if [[ -z "$OPENSCQ30_SKIP_SHELL_COMPLETIONS" ]]; then
+if [[ -z "${OPENSCQ30_SKIP_SHELL_COMPLETIONS:-}" ]]; then
     if [[ -d "$install_prefix/share/bash-completions/completions" ]]; then
         echo Installing bash completions
         "../target/release/openscq30_cli$bin_ext" completions bash > "$install_prefix/share/bash-completions/completions/openscq30_cli"
