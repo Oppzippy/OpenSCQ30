3a30845504d16334886b8cf2489eb3ded2620241
Kyle Scheuing <kyle@kylescheuing.com>
2024-08-30 01:14:22 -0400

    refactor(lib): remove unused import

--- lib/src/soundcore_device/connection/windows.rs
+++ lib/src/soundcore_device/connection/windows.rs
@@ -5,7 +5,6 @@ mod windows_mac_addess;
 
 pub use windows_connection::*;
 pub use windows_connection_registry::*;
-pub use windows_error::*;
 pub(crate) use windows_mac_addess::*;
 
 pub(crate) async fn new_connection_registry() -> crate::Result<WindowsConnectionRegistry> {
