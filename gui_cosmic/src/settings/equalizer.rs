b824554c156dc03e761dd3ac9f2a39bec8433ab5
Kyle Scheuing <kyle@kylescheuing.com>
2025-02-11 04:11:43 -0500

    gui_cosmic: fix equalizer text layout issues

--- gui_cosmic/src/settings/equalizer.rs
+++ gui_cosmic/src/settings/equalizer.rs
@@ -1,6 +1,11 @@
+use std::{
+    borrow::Cow,
+    ops::{Add, Sub},
+};
+
 use cosmic::{
-    iced::{alignment, Length},
-    widget, Element,
+    iced::{alignment, Alignment, Border, Length, Shadow},
+    theme, widget, Apply, Element,
 };
 use openscq30_lib::api::settings::Equalizer;
 
@@ -29,7 +34,7 @@ where
     .into()
 }
 
-pub fn horizontal_equalizer<'a, M>(
+pub fn vertical_equalizer<'a, M>(
     setting: &Equalizer,
     value: &[i16],
     on_change: impl Fn(u8, i16) -> M + 'static + Clone,
@@ -37,14 +42,24 @@ pub fn horizontal_equalizer<'a, M>(
 where
     M: Clone + 'static,
 {
-    widget::column()
+    widget::row()
         .extend(setting.band_hz.iter().cloned().enumerate().map(|(i, hz)| {
-            widget::row()
+            widget::column()
                 .width(Length::Fill)
-                .align_y(alignment::Vertical::Center)
-                .spacing(8)
-                .push(widget::text::text(fl!("hz", hz = hz)))
-                .push(widget::spin_button(
+                .align_x(alignment::Horizontal::Center)
+                .spacing(10)
+                .push(widget::vertical_slider(
+                    setting.min..=setting.max,
+                    value
+                        .get(i as usize)
+                        .cloned()
+                        .unwrap_or((setting.min + setting.max) / 2),
+                    {
+                        let on_change = on_change.clone();
+                        move |band_value| on_change(i as u8, band_value)
+                    },
+                ))
+                .push(vertical_spin_button(
                     {
                         let divisor = 10i16.pow(setting.fraction_digits as u32);
                         let db_integer_portion = value[i as usize] / divisor;
@@ -60,6 +75,46 @@ where
                         let on_change = on_change.clone();
                         move |band_value| on_change(i as u8, band_value)
                     },
+                ))
+                .push(widget::text::title4(fl!("hz", hz = hz)))
+                .into()
+        }))
+        .into()
+}
+
+pub fn horizontal_equalizer<'a, M>(
+    setting: &Equalizer,
+    value: &[i16],
+    on_change: impl Fn(u8, i16) -> M + 'static + Clone,
+) -> Element<'a, M>
+where
+    M: Clone + 'static,
+{
+    widget::column()
+        .spacing(8)
+        .extend(setting.band_hz.iter().cloned().enumerate().map(|(i, hz)| {
+            widget::row()
+                .width(Length::Fill)
+                .align_y(alignment::Vertical::Center)
+                .spacing(8)
+                .push(widget::text::title4(fl!("hz", hz = hz)).width(80))
+                .push(horizontal_spin_button(
+                    {
+                        let divisor = 10i16.pow(setting.fraction_digits as u32);
+                        let db_integer_portion = value[i as usize] / divisor;
+                        let db_decimal_portion = (value[i as usize] % divisor).abs();
+                        let decimal_db = format!("{db_integer_portion}.{db_decimal_portion}");
+                        fl!("db", db = decimal_db)
+                    },
+                    value[i as usize],
+                    1,
+                    setting.min,
+                    setting.max,
+                    {
+                        let on_change = on_change.clone();
+                        move |band_value| on_change(i as u8, band_value)
+                    },
+                    80,
                 ))
                 .push(widget::slider(
                     setting.min..=setting.max,
@@ -77,50 +132,116 @@ where
         .into()
 }
 
-pub fn vertical_equalizer<'a, M>(
-    setting: &Equalizer,
-    value: &[i16],
-    on_change: impl Fn(u8, i16) -> M + 'static + Clone,
+// Taken from libcosmic and modified to fix text not taking enough width, causing it to go to the next line
+pub fn horizontal_spin_button<'a, T, M>(
+    label: impl Into<Cow<'a, str>> + 'a,
+    value: T,
+    step: T,
+    min: T,
+    max: T,
+    on_press: impl Fn(T) -> M + 'static,
+    width: impl Into<Length>,
 ) -> Element<'a, M>
 where
     M: Clone + 'static,
+    T: Copy + Sub<Output = T> + Add<Output = T> + PartialOrd,
 {
-    widget::row()
-        .extend(setting.band_hz.iter().cloned().enumerate().map(|(i, hz)| {
-            widget::column()
-                .width(Length::Fill)
-                .align_x(alignment::Horizontal::Center)
-                .spacing(8)
-                .push(widget::vertical_slider(
-                    setting.min..=setting.max,
-                    value
-                        .get(i as usize)
-                        .cloned()
-                        .unwrap_or((setting.min + setting.max) / 2),
-                    {
-                        let on_change = on_change.clone();
-                        move |band_value| on_change(i as u8, band_value)
-                    },
-                ))
-                .push(widget::vertical_spin_button(
-                    {
-                        let divisor = 10i16.pow(setting.fraction_digits as u32);
-                        let db_integer_portion = value[i as usize] / divisor;
-                        let db_decimal_portion = (value[i as usize] % divisor).abs();
-                        let decimal_db = format!("{db_integer_portion}.{db_decimal_portion}");
-                        fl!("db", db = decimal_db)
-                    },
-                    value[i as usize],
-                    1,
-                    setting.min,
-                    setting.max,
-                    {
-                        let on_change = on_change.clone();
-                        move |band_value| on_change(i as u8, band_value)
-                    },
-                ))
-                .push(widget::text::text(fl!("hz", hz = hz)))
-                .into()
-        }))
+    let decrement_button = widget::icon::from_name("list-remove-symbolic")
+        .apply(widget::button::icon)
+        .on_press((on_press)(decrement::<T>(value, step, min, max)));
+
+    let increment_button = widget::icon::from_name("list-add-symbolic")
+        .apply(widget::button::icon)
+        .on_press((on_press)(increment::<T>(value, step, min, max)));
+
+    let label = widget::text::title4(label)
+        .apply(widget::container)
+        .width(width)
+        .align_x(Alignment::Center)
+        .align_y(Alignment::Center);
+
+    widget::row::with_capacity(3)
+        .push(decrement_button)
+        .push(label)
+        .push(increment_button)
+        .align_y(Alignment::Center)
+        .apply(widget::container)
+        .class(theme::Container::custom(container_style))
         .into()
+}
+
+pub fn vertical_spin_button<'a, T, M>(
+    label: impl Into<Cow<'a, str>> + 'a,
+    value: T,
+    step: T,
+    min: T,
+    max: T,
+    on_press: impl Fn(T) -> M + 'static,
+) -> Element<'a, M>
+where
+    M: Clone + 'static,
+    T: Copy + Sub<Output = T> + Add<Output = T> + PartialOrd,
+{
+    let decrement_button = widget::icon::from_name("list-remove-symbolic")
+        .apply(widget::button::icon)
+        .on_press((on_press)(decrement::<T>(value, step, min, max)));
+
+    let increment_button = widget::icon::from_name("list-add-symbolic")
+        .apply(widget::button::icon)
+        .on_press((on_press)(increment::<T>(value, step, min, max)));
+
+    let label = widget::text::title4(label)
+        .apply(widget::container)
+        .width(Length::Fill)
+        .align_x(Alignment::Center);
+
+    widget::column::with_capacity(3)
+        .push(increment_button)
+        .push(label)
+        .push(decrement_button)
+        .align_x(Alignment::Center)
+        .apply(widget::container)
+        .class(theme::Container::custom(container_style))
+        .into()
+}
+
+fn increment<T>(value: T, step: T, _min: T, max: T) -> T
+where
+    T: Copy + Sub<Output = T> + Add<Output = T> + PartialOrd,
+{
+    if value > max - step {
+        max
+    } else {
+        value + step
+    }
+}
+
+fn decrement<T>(value: T, step: T, min: T, _max: T) -> T
+where
+    T: Copy + Sub<Output = T> + Add<Output = T> + PartialOrd,
+{
+    if value < min + step {
+        min
+    } else {
+        value - step
+    }
+}
+
+fn container_style(theme: &cosmic::Theme) -> cosmic::iced::widget::container::Style {
+    let cosmic_theme = &theme.cosmic();
+    let mut neutral_10 = cosmic_theme.palette.neutral_10;
+    neutral_10.alpha = 0.1;
+    let accent = &cosmic_theme.accent;
+    let corners = &cosmic_theme.corner_radii;
+    cosmic::iced::widget::container::Style {
+        icon_color: Some(accent.base.into()),
+        text_color: Some(cosmic_theme.palette.neutral_10.into()),
+        background: None,
+        border: Border {
+            radius: corners.radius_s.into(),
+            width: 0.0,
+            color: accent.base.into(),
+        },
+        shadow: Shadow::default(),
+    }
 }
