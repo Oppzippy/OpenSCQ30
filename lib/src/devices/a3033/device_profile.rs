fff67074f762d7bc778d27e2cde2152b6cccee4c
Kyle Scheuing <kyle@kylescheuing.com>
2025-03-04 17:08:29 -0500

    lib: implement a3033 with new api

--- lib/src/devices/a3033/device_profile.rs
+++ lib/src/devices/a3033/device_profile.rs
@@ -1,10 +1,32 @@
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use tokio::sync::watch;
+
 use crate::{
+    api::{
+        connection::{RfcommBackend, RfcommConnection},
+        device::OpenSCQ30Device,
+        settings::{CategoryId, Setting, SettingId, Value},
+    },
     device_profile::{DeviceFeatures, DeviceProfile},
-    devices::standard::implementation::StandardImplementation,
-    soundcore_device::device_model::DeviceModel,
+    devices::standard::{
+        demo::DemoConnectionRegistry,
+        device::{SoundcoreDevice, SoundcoreDeviceRegistry},
+        implementation::StandardImplementation,
+        modules::{ModuleCollection, ModuleCollectionSpawnPacketHandlerExt},
+        packets::{
+            inbound::TryIntoInboundPacket,
+            outbound::{OutboundPacketBytesExt, RequestStatePacket},
+        },
+    },
+    soundcore_device::{
+        device::packet_io_controller::PacketIOController, device_model::DeviceModel,
+    },
+    storage::OpenSCQ30Database,
 };
 
-use super::packets::A3033StateUpdatePacket;
+use super::{packets::A3033StateUpdatePacket, state::A3033State};
 
 pub(crate) const A3033_DEVICE_PROFILE: DeviceProfile = DeviceProfile {
     features: DeviceFeatures {
@@ -23,3 +45,116 @@ pub(crate) const A3033_DEVICE_PROFILE: DeviceProfile = DeviceProfile {
     compatible_models: &[DeviceModel::SoundcoreA3033],
     implementation: || StandardImplementation::new::<A3033StateUpdatePacket>(),
 };
+
+pub fn device_registry<B: RfcommBackend>(
+    backend: B,
+    database: Arc<OpenSCQ30Database>,
+    device_model: DeviceModel,
+) -> SoundcoreDeviceRegistry<B, A3033Device<B::ConnectionType>> {
+    SoundcoreDeviceRegistry::new(backend, database, device_model)
+}
+
+pub fn demo_device_registry(
+    database: Arc<OpenSCQ30Database>,
+    device_model: DeviceModel,
+) -> SoundcoreDeviceRegistry<
+    DemoConnectionRegistry,
+    A3033Device<<DemoConnectionRegistry as RfcommBackend>::ConnectionType>,
+> {
+    SoundcoreDeviceRegistry::new(
+        DemoConnectionRegistry::new(
+            device_model.to_string(),
+            A3033StateUpdatePacket::default().bytes(),
+        ),
+        database,
+        device_model,
+    )
+}
+
+pub struct A3033Device<ConnectionType: RfcommConnection + Send + Sync> {
+    device_model: DeviceModel,
+    state_sender: watch::Sender<A3033State>,
+    module_collection: Arc<ModuleCollection<A3033State>>,
+    _packet_io_controller: Arc<PacketIOController<ConnectionType>>,
+}
+
+impl<ConnectionType> SoundcoreDevice<ConnectionType> for A3033Device<ConnectionType>
+where
+    ConnectionType: RfcommConnection + 'static + Send + Sync,
+{
+    async fn new(
+        database: Arc<OpenSCQ30Database>,
+        connection: ConnectionType,
+        device_model: DeviceModel,
+    ) -> crate::Result<Self> {
+        let (packet_io_controller, packet_receiver) =
+            PacketIOController::<ConnectionType>::new(Arc::new(connection)).await?;
+        let packet_io_controller = Arc::new(packet_io_controller);
+        let state_update_packet: A3033StateUpdatePacket = packet_io_controller
+            .send(&RequestStatePacket::new().into())
+            .await?
+            .try_into_inbound_packet()?;
+        let (state_sender, _) = watch::channel::<A3033State>(state_update_packet.into());
+
+        let mut module_collection = ModuleCollection::<A3033State>::default();
+        module_collection.add_state_update();
+        module_collection
+            .add_equalizer(packet_io_controller.clone(), database, device_model, false)
+            .await;
+
+        let module_collection = Arc::new(module_collection);
+        module_collection.spawn_packet_handler(state_sender.clone(), packet_receiver);
+
+        Ok(Self {
+            device_model,
+            state_sender,
+            _packet_io_controller: packet_io_controller,
+            module_collection,
+        })
+    }
+}
+
+#[async_trait]
+impl<ConnectionType> OpenSCQ30Device for A3033Device<ConnectionType>
+where
+    ConnectionType: RfcommConnection + 'static + Send + Sync,
+{
+    fn model(&self) -> DeviceModel {
+        self.device_model
+    }
+
+    fn categories(&self) -> Vec<CategoryId> {
+        self.module_collection.setting_manager.categories().to_vec()
+    }
+
+    fn settings_in_category(&self, category_id: &CategoryId) -> Vec<SettingId> {
+        self.module_collection.setting_manager.category(category_id)
+    }
+
+    fn setting(&self, setting_id: &SettingId) -> Option<Setting> {
+        let state = self.state_sender.borrow().to_owned();
+        self.module_collection
+            .setting_manager
+            .get(&state, setting_id)
+    }
+
+    async fn set_setting_values(
+        &self,
+        setting_values: Vec<(SettingId, Value)>,
+    ) -> crate::Result<()> {
+        let mut target_state = self.state_sender.borrow().clone();
+        for (setting_id, value) in setting_values {
+            self.module_collection
+                .setting_manager
+                .set(&mut target_state, &setting_id, value)
+                .await
+                .unwrap()?;
+        }
+        for modifier in &self.module_collection.state_modifiers {
+            modifier
+                .move_to_state(&self.state_sender, &target_state)
+                .await?;
+        }
+        Ok(())
+    }
+}
--- lib/src/devices/a3033/packets/state_update_packet.rs
+++ lib/src/devices/a3033/packets/state_update_packet.rs
@@ -1,29 +1,39 @@
+use async_trait::async_trait;
 use nom::{
     IResult,
     combinator::{all_consuming, map},
     error::{ContextError, ParseError, context},
     sequence::tuple,
 };
+use tokio::sync::watch;
 
-use crate::devices::{
-    a3033::device_profile::A3033_DEVICE_PROFILE,
-    standard::{
-        packets::{
-            inbound::{InboundPacket, state_update_packet::StateUpdatePacket},
-            parsing::take_bool,
+use crate::{
+    devices::{
+        a3033::{device_profile::A3033_DEVICE_PROFILE, state::A3033State},
+        standard::{
+            modules::ModuleCollection,
+            packet_manager::PacketHandler,
+            packets::{
+                inbound::{
+                    InboundPacket, TryIntoInboundPacket, state_update_packet::StateUpdatePacket,
+                },
+                outbound::OutboundPacket,
+                parsing::take_bool,
+            },
+            structures::{EqualizerConfiguration, FirmwareVersion, SerialNumber, SingleBattery},
         },
-        structures::{EqualizerConfiguration, FirmwareVersion, SerialNumber, SingleBattery},
     },
+    soundcore_device::device::Packet,
 };
 
 // A3033 and A3033EU
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Default)]
 pub struct A3033StateUpdatePacket {
-    battery: SingleBattery,
-    equalizer_configuration: EqualizerConfiguration,
-    firmware_version: FirmwareVersion,
-    serial_number: SerialNumber,
-    wear_detection: bool,
+    pub battery: SingleBattery,
+    pub equalizer_configuration: EqualizerConfiguration,
+    pub firmware_version: FirmwareVersion,
+    pub serial_number: SerialNumber,
+    pub wear_detection: bool,
 }
 
 impl From<A3033StateUpdatePacket> for StateUpdatePacket {
@@ -81,5 +91,46 @@ impl InboundPacket for A3033StateUpdatePacket {
                 },
             )),
         )(input)
+    }
+}
+
+impl OutboundPacket for A3033StateUpdatePacket {
+    fn command(&self) -> crate::devices::standard::structures::Command {
+        StateUpdatePacket::command()
+    }
+
+    fn body(&self) -> Vec<u8> {
+        [self.battery.is_charging as u8, self.battery.level.0]
+            .into_iter()
+            .chain(self.equalizer_configuration.profile_id().to_le_bytes())
+            .chain(self.equalizer_configuration.volume_adjustments().bytes())
+            .chain(self.firmware_version.to_string().into_bytes())
+            .chain(self.serial_number.0.as_bytes().iter().cloned())
+            .chain([self.wear_detection as u8])
+            .collect()
+    }
+}
+
+struct StateUpdatePacketHandler {}
+
+#[async_trait]
+impl PacketHandler<A3033State> for StateUpdatePacketHandler {
+    async fn handle_packet(
+        &self,
+        state: &watch::Sender<A3033State>,
+        packet: &Packet,
+    ) -> crate::Result<()> {
+        let packet: A3033StateUpdatePacket = packet.try_into_inbound_packet()?;
+        state.send_modify(|state| *state = packet.into());
+        Ok(())
+    }
+}
+
+impl ModuleCollection<A3033State> {
+    pub fn add_state_update(&mut self) {
+        self.packet_handlers.set_handler(
+            StateUpdatePacket::command(),
+            Box::new(StateUpdatePacketHandler {}),
+        );
     }
 }
