f41f4089bb989701671ef9a36d0c9b2f2812a724
Kyle Scheuing <kyle@kylescheuing.com>
2025-04-17 01:46:36 -0400

    refactor(cli): split up pair functions

--- cli/src/cli/pair.rs
+++ cli/src/cli/pair.rs
@@ -1,6 +1,6 @@
 use clap::ArgMatches;
 use macaddr::MacAddr6;
-use openscq30_lib::{devices::DeviceModel, storage::PairedDevice};
+use openscq30_lib::{api::OpenSCQ30Session, devices::DeviceModel, storage::PairedDevice};
 use tabled::{Table, Tabled};
 
 use crate::openscq30_session;
@@ -8,43 +8,52 @@ use crate::openscq30_session;
 pub async fn handle(matches: &ArgMatches) -> anyhow::Result<()> {
     let session = openscq30_session().await?;
     match matches.subcommand().unwrap() {
-        ("add", matches) => {
-            session
-                .pair(PairedDevice {
-                    name: matches.get_one::<String>("name").unwrap().to_owned(),
-                    mac_address: matches
-                        .get_one::<MacAddr6>("mac-address")
-                        .unwrap()
-                        .to_owned(),
-                    model: matches.get_one::<DeviceModel>("model").unwrap().to_owned(),
-                })
-                .await?;
-            println!("Paired");
-        }
-        ("remove", matches) => {
-            session
-                .unpair(
-                    matches
-                        .get_one::<MacAddr6>("mac-address")
-                        .unwrap()
-                        .to_owned(),
-                )
-                .await?;
-            println!("Unpaired");
-        }
-        ("list", _matches) => {
-            let mut table = Table::new(
-                session
-                    .paired_devices()
-                    .await?
-                    .into_iter()
-                    .map(PairedDeviceTableItem::from),
-            );
-            crate::fmt::apply_tabled_settings(&mut table);
-            println!("{table}");
-        }
+        ("add", matches) => handle_add(matches, &session).await?,
+        ("remove", matches) => handle_remove(matches, &session).await?,
+        ("list", _matches) => handle_list(matches, &session).await?,
         _ => unreachable!(),
     }
+    Ok(())
+}
+
+async fn handle_add(matches: &ArgMatches, session: &OpenSCQ30Session) -> anyhow::Result<()> {
+    session
+        .pair(PairedDevice {
+            name: matches.get_one::<String>("name").unwrap().to_owned(),
+            mac_address: matches
+                .get_one::<MacAddr6>("mac-address")
+                .unwrap()
+                .to_owned(),
+            model: matches.get_one::<DeviceModel>("model").unwrap().to_owned(),
+        })
+        .await?;
+    println!("Paired");
+    Ok(())
+}
+
+async fn handle_remove(matches: &ArgMatches, session: &OpenSCQ30Session) -> anyhow::Result<()> {
+    session
+        .unpair(
+            matches
+                .get_one::<MacAddr6>("mac-address")
+                .unwrap()
+                .to_owned(),
+        )
+        .await?;
+    println!("Unpaired");
+    Ok(())
+}
+
+async fn handle_list(_matches: &ArgMatches, session: &OpenSCQ30Session) -> anyhow::Result<()> {
+    let mut table = Table::new(
+        session
+            .paired_devices()
+            .await?
+            .into_iter()
+            .map(PairedDeviceTableItem::from),
+    );
+    crate::fmt::apply_tabled_settings(&mut table);
+    println!("{table}");
     Ok(())
 }
 
