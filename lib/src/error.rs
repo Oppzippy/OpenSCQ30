d8aff6eb12516dbd5c39f422f0c326d5c842f972
Kyle Scheuing <kyle@kylescheuing.com>
2025-04-20 10:30:37 -0400

    refactor(lib): remove unused error variants

--- lib/src/error.rs
+++ lib/src/error.rs
@@ -1,8 +1,5 @@
 use std::panic::Location;
 
-use macaddr::MacAddr6;
-use uuid::Uuid;
-
 use crate::api::settings::ValueError;
 
 type InnerError = Box<dyn std::error::Error + Send + Sync>;
@@ -12,40 +9,11 @@ pub enum Error {
     #[error("device not found: {source:?}")]
     DeviceNotFound { source: Option<InnerError> },
 
-    #[error("not connected: {source:?}")]
-    NotConnected { source: Option<InnerError> },
-
-    #[error("name of device with mac address `{mac_address}` not found")]
-    NameNotFound {
-        mac_address: MacAddr6,
-        source: Option<InnerError>,
-    },
-
-    #[error("characteristic `{uuid}` not found: {source:?}")]
-    CharacteristicNotFound {
-        uuid: Uuid,
-        source: Option<InnerError>,
-    },
-
-    #[error("service not found: {source:?}")]
-    ServiceNotFound { source: Option<InnerError> },
-
-    #[error("feature not supported: {feature_name}")]
-    FeatureNotSupported { feature_name: &'static str },
-
-    #[error("missing necessary data from headphones: {name}")]
-    MissingData { name: &'static str },
-
     #[error("write failed: {source:?}")]
     WriteFailed { source: InnerError },
 
-    #[error("incomplete state: {message:?}")]
-    IncompleteStateError { message: &'static str },
     #[error("timed out: {action}")]
     TimedOut { action: &'static str },
-
-    #[error("parse error: {message:?}")]
-    ParseError { message: String },
 
     #[error("bluetooth adapter not available: {source:?}")]
     BluetoothAdapterNotAvailable { source: Option<InnerError> },
