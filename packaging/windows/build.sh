90a2341023e097331bac47d33df7f8c4e0d98f8c
Kyle Scheuing <kyle@kylescheuing.com>
2025-05-02 19:21:32 -0400

    build(windows): update build script

--- packaging/windows/build.sh
+++ packaging/windows/build.sh
@@ -4,43 +4,18 @@ set -euo pipefail
 script_path="$(readlink -f -- "$0")"
 script_dir="$(dirname -- "$script_path")"
 project_root="$script_dir/../.."
-input_exe="$project_root/target/release/openscq30_gui.exe"
+input_exe="$project_root/target/release/openscq30-gui.exe"
 
-package_type="ucrt64"
-output_dir="$script_dir/root"
+# Fetch dependencies
+if [[ ! -d "$script_dir/dependencies/cosmic-icons" ]]; then
+    git clone --depth 1 https://github.com/pop-os/cosmic-icons.git "$script_dir/dependencies/cosmic-icons"
+fi
 
-# Install OpenSCQ30
-just "$project_root/gui/" install "$output_dir"
+# Copy files into installation directory
+mkdir --parents "$script_dir/root/bin"
+mkdir --parents "$script_dir/root/share/icons/Cosmic/scalable"
 
-# Determine what dlls are required, and which packages those dlls belong to
-dependencies=""
-list=$(ldd "$input_exe" | grep "/$package_type" | sed "s/.dll.*/.dll/")
-for dll in $list;
-do
-    package=$(pacman -Qo "$dll" | awk '{ print $5 }')
-    dependencies="$dependencies"$'\n'"$package"
-done
-dependencies=$(printf "%s" "$dependencies" | sort | uniq)
-echo "packages: $dependencies"
-
-# Find all dll files belonging to packages that own required dll files
-# shellcheck disable=SC2086
-files=$(pacman -Ql $dependencies | sed -E "s/^[^ ]+ //" | grep -E "^\/$package_type\/bin/.*\.dll" | sort | uniq)
-echo "files: $files"
-
-# Install the files, stripping the /ucrt64 prefix
-for file in $files; do
-    if [[ -f "$file" ]]; then
-        file_without_prefix=$(echo "$file" | sed -E "s/^\/$package_type//")
-        output_file="$output_dir/$file_without_prefix"
-        output_file_dir=$(dirname "$output_file")
-        mkdir -p "$output_file_dir"
-        cp "$file" "$output_file"
-    fi
-done
-
-# Install other dependencies
-mkdir -p "$output_dir/share/glib-2.0"
-cp -r "/$package_type/bin/gdbus.exe" "$output_dir/bin/"
-cp -r "/$package_type/share/locale" "$output_dir/share/"
-cp -r "/$package_type/share/glib-2.0/schemas" "$output_dir/share/glib-2.0/"
+cp "$input_exe" "$script_dir/root/bin/"
+rsync --archive --delete \
+    "$script_dir/dependencies/cosmic-icons/freedesktop/scalable/" \
+    "$script_dir/root/share/icons/Cosmic/scalable/"
