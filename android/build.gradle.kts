235c9d1c66ac0db73ae8280454242a872d8a14a7
Kyle Scheuing <kyle@kylescheuing.com>
2025-07-16 09:21:37 -0400

    deps(android): update AGP

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.10.1" apply false
-    id("com.android.library") version "8.10.1" apply false
+    id("com.android.application") version "8.11.1" apply false
+    id("com.android.library") version "8.11.1" apply false
 
     val kotlinVersion = "2.1.21"
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
