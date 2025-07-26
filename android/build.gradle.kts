c63e896777e968dd28d18c8af6d5214d396c0da0
Kyle Scheuing <kyle@kylescheuing.com>
2025-07-26 19:33:54 -0400

    deps(android): update kotlin to 2.2.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,10 +3,10 @@ plugins {
     id("com.android.application") version "8.11.1" apply false
     id("com.android.library") version "8.11.1" apply false
 
-    val kotlinVersion = "2.1.21"
+    val kotlinVersion = "2.2.0"
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
     id("org.jetbrains.kotlin.plugin.compose") version kotlinVersion apply false
-    id("com.google.devtools.ksp") version "$kotlinVersion-2.0.1" apply false
+    id("com.google.devtools.ksp") version "$kotlinVersion-2.0.2" apply false
     kotlin("plugin.serialization") version kotlinVersion
 
     id("com.google.dagger.hilt.android") version "2.57" apply false
