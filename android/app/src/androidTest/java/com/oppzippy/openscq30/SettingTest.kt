0c96a2ae42f540374cb04212a8a92f5e90c3703d
Kyle Scheuing <kyle@kylescheuing.com>
2025-07-27 15:45:32 -0400

    test(android): add select and optional select tests

--- android/app/src/androidTest/java/com/oppzippy/openscq30/SettingTest.kt
+++ android/app/src/androidTest/java/com/oppzippy/openscq30/SettingTest.kt
@@ -3,11 +3,13 @@ package com.oppzippy.openscq30
 import androidx.compose.ui.test.assertIsOff
 import androidx.compose.ui.test.assertIsOn
 import androidx.compose.ui.test.click
+import androidx.compose.ui.test.hasAnyAncestor
+import androidx.compose.ui.test.hasTextExactly
+import androidx.compose.ui.test.isDialog
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTouchInput
-import androidx.compose.ui.test.swipeRight
 import com.oppzippy.openscq30.actions.addAndConnectToDemoDevice
 import com.oppzippy.openscq30.extensions.assertRangeValueApproxEquals
 import com.oppzippy.openscq30.lib.bindings.translateCategoryId
@@ -36,5 +38,26 @@ class SettingTest : OpenSCQ30RootTestBase() {
             .assertRangeValueApproxEquals(0f)
             .performTouchInput { click(centerRight) }
             .assertRangeValueApproxEquals(10f)
+    }
+
+    @Test
+    fun testSelect() {
+        addAndConnectToDemoDevice(composeRule, translateDeviceModel("SoundcoreA3951"))
+        composeRule.onNodeWithText(translateCategoryId("soundModes")).performClick()
+        composeRule.onNodeWithText("Noise Canceling").performClick()
+        composeRule.onNodeWithText("Normal").performClick().assertExists()
+        composeRule.onNodeWithText("Noise Canceling").assertDoesNotExist()
+    }
+
+    @Test
+    fun testOptionalSelect() {
+        addAndConnectToDemoDevice(composeRule, translateDeviceModel("SoundcoreA3951"))
+        composeRule.onNodeWithText(translateCategoryId("equalizer")).performClick()
+        composeRule.onNodeWithText("Soundcore Signature").performClick()
+        composeRule.waitForIdle()
+        composeRule.onNode(hasTextExactly("None") and hasAnyAncestor(isDialog()))
+            .performClick()
+        composeRule.onNode(hasTextExactly("None") and !hasAnyAncestor(isDialog()))
+        composeRule.onNodeWithText("Soundcore Signature").assertDoesNotExist()
     }
 }
