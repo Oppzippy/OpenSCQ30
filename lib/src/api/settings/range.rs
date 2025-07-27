754e82d312a57574d0b0d65ab7013ec36c543aad
Kyle Scheuing <kyle@kylescheuing.com>
2025-07-26 20:31:17 -0400

    fix(lib)!: flatten nested range struct when serializing
    This is a breaking change since it will break existing quick presets.

--- lib/src/api/settings/range.rs
+++ lib/src/api/settings/range.rs
@@ -5,6 +5,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Range<T> {
+    #[serde(flatten)]
     pub range: RangeInclusive<T>,
     pub step: T,
 }
