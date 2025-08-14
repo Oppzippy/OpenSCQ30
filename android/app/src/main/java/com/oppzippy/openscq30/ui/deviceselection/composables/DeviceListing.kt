b762f868b1278012b3ad17cacf1cccb224efa6f8
Kyle Scheuing <kyle@kylescheuing.com>
2025-08-14 04:45:13 -0400

    refactor(android): remove unused import and translation

--- android/app/src/main/java/com/oppzippy/openscq30/ui/deviceselection/composables/DeviceListing.kt
+++ android/app/src/main/java/com/oppzippy/openscq30/ui/deviceselection/composables/DeviceListing.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
