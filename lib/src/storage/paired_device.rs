c710267d0ecf2589a31ca2dbcdc9edd4493e040e
Kyle Scheuing <kyle@kylescheuing.com>
2025-08-18 04:00:25 -0400

    test(lib): add storage/paired_device tests

--- lib/src/storage/paired_device.rs
+++ lib/src/storage/paired_device.rs
@@ -10,7 +10,7 @@ use super::{
     type_conversions::{SqliteDeviceModel, SqliteMacAddr6},
 };
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(PartialEq, Eq, Hash, Copy, Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct PairedDevice {
     #[serde(with = "crate::serialization::mac_addr")]
@@ -112,4 +112,142 @@ pub fn delete(connection: &Connection, mac_address: MacAddr6) -> Result<(), Erro
         (SqliteMacAddr6(mac_address),),
     )?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+
+    use macaddr::MacAddr6;
+
+    use crate::storage::{OpenSCQ30Database, PairedDevice};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_fetch_all() {
+        let db = OpenSCQ30Database::new_in_memory().await.unwrap();
+        let paired_devices = [
+            PairedDevice {
+                mac_address: MacAddr6::new(0, 0, 0, 0, 0, 1),
+                model: DeviceModel::SoundcoreA3028,
+                is_demo: false,
+            },
+            PairedDevice {
+                mac_address: MacAddr6::new(0, 0, 0, 0, 0, 2),
+                model: DeviceModel::SoundcoreA3033,
+                is_demo: false,
+            },
+            PairedDevice {
+                mac_address: MacAddr6::new(0, 0, 0, 0, 0, 3),
+                model: DeviceModel::SoundcoreA3029,
+                is_demo: false,
+            },
+        ];
+        for device in &paired_devices {
+            db.insert_paired_device(*device).await.unwrap();
+        }
+        let fetched_devices = db.fetch_all_paired_devices().await.unwrap();
+
+        assert_eq!(
+            HashSet::from(paired_devices),
+            HashSet::from_iter(fetched_devices)
+        );
+    }
+
+    #[tokio::test]
+    async fn test_fetch() {
+        let db = OpenSCQ30Database::new_in_memory().await.unwrap();
+        let paired_devices = [
+            PairedDevice {
+                mac_address: MacAddr6::new(0, 0, 0, 0, 0, 1),
+                model: DeviceModel::SoundcoreA3028,
+                is_demo: false,
+            },
+            PairedDevice {
+                mac_address: MacAddr6::new(0, 0, 0, 0, 0, 2),
+                model: DeviceModel::SoundcoreA3033,
+                is_demo: false,
+            },
+            PairedDevice {
+                mac_address: MacAddr6::new(0, 0, 0, 0, 0, 3),
+                model: DeviceModel::SoundcoreA3029,
+                is_demo: false,
+            },
+        ];
+        for device in paired_devices {
+            db.insert_paired_device(device).await.unwrap();
+        }
+
+        for device in paired_devices {
+            let fetched_device = db.fetch_paired_device(device.mac_address).await.unwrap();
+            assert_eq!(Some(device), fetched_device);
+        }
+    }
+
+    #[tokio::test]
+    async fn test_upsert() {
+        let db = OpenSCQ30Database::new_in_memory().await.unwrap();
+        let paired_devices = [
+            PairedDevice {
+                mac_address: MacAddr6::new(0, 0, 0, 0, 0, 1),
+                model: DeviceModel::SoundcoreA3028,
+                is_demo: false,
+            },
+            PairedDevice {
+                mac_address: MacAddr6::new(0, 0, 0, 0, 0, 2),
+                model: DeviceModel::SoundcoreA3033,
+                is_demo: false,
+            },
+        ];
+
+        // Insert one to be overwritten
+        db.insert_paired_device(PairedDevice {
+            mac_address: MacAddr6::new(0, 0, 0, 0, 0, 1),
+            model: DeviceModel::SoundcoreA3004,
+            is_demo: true,
+        })
+        .await
+        .unwrap();
+        for device in &paired_devices {
+            db.upsert_paired_device(*device).await.unwrap();
+        }
+        let fetched_devices = db.fetch_all_paired_devices().await.unwrap();
+
+        assert_eq!(
+            HashSet::from(paired_devices),
+            HashSet::from_iter(fetched_devices)
+        );
+    }
+
+    #[tokio::test]
+    async fn test_delete() {
+        let db = OpenSCQ30Database::new_in_memory().await.unwrap();
+        let paired_devices = [
+            PairedDevice {
+                mac_address: MacAddr6::new(0, 0, 0, 0, 0, 1),
+                model: DeviceModel::SoundcoreA3028,
+                is_demo: false,
+            },
+            PairedDevice {
+                mac_address: MacAddr6::new(0, 0, 0, 0, 0, 2),
+                model: DeviceModel::SoundcoreA3033,
+                is_demo: false,
+            },
+        ];
+        for device in &paired_devices {
+            db.upsert_paired_device(*device).await.unwrap();
+        }
+
+        // Insert one to be overwritten
+        db.delete_paired_device(paired_devices[0].mac_address)
+            .await
+            .unwrap();
+        let fetched_devices = db.fetch_all_paired_devices().await.unwrap();
+
+        assert_eq!(
+            HashSet::from([paired_devices[1]]),
+            HashSet::from_iter(fetched_devices)
+        );
+    }
 }
