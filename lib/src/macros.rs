699de2a33ffe9ada2b548d1281cd8327e801cf99
Kyle Scheuing <kyle@kylescheuing.com>
2025-09-03 10:45:24 -0400

    docs(lib): add documentation for macros
    This only includes general purpose macros, not the soundcore specific
    ones.

--- lib/src/macros.rs
+++ lib/src/macros.rs
@@ -1,3 +1,27 @@
+/// Implements `From` for error types that wrap a source error along with the
+/// source code location that the error occurred.
+///
+/// # Example
+/// ```ignore
+/// use crate::macros::impl_from_source_error_with_location;
+///
+/// enum OuterError {
+///     InnerError {
+///         source: InnerError,
+///         location: &'static std::panic::Location<'static>,
+///     },
+/// }
+///
+/// struct InnerError;
+///
+/// impl_from_source_error_with_location!(OuterError::InnerError(InnerError));
+///
+/// fn fail() -> Result<(), OuterError> {
+///     Err(OuterError::from(InnerError))
+/// }
+///
+/// assert!(matches!(fail().unwrap_err(), OuterError::InnerError { .. }))
+/// ```
 macro_rules! impl_from_source_error_with_location {
     ($target_error:ident::$target_variant:ident($source_error:ty)) => {
         impl From<$source_error> for $target_error {
@@ -13,6 +37,32 @@ macro_rules! impl_from_source_error_with_location {
 }
 pub(crate) use impl_from_source_error_with_location;
 
+/// Generates an enum that is a subset of an existing enum, along with a TryFrom implementation for superset to subset,
+/// and a From implementation for subset to superset.
+///
+/// # Examples
+///
+/// ```ignore
+/// use crate::macros::enum_subset;
+///
+/// #[derive(Debug, PartialEq)]
+/// enum Superset {
+///     A,
+///     B,
+/// }
+///
+/// enum_subset! {
+///     Superset,
+///     #[derive(Debug, PartialEq)]
+///     enum Subset {
+///         A,
+///     }
+/// };
+///
+/// assert_eq!(Superset::from(Subset::A), Superset::A);
+/// assert_eq!(Subset::try_from(Superset::A), Superset::A);
+/// assert!(Subset::try_from(Superset::B).is_err());
+/// ```
 macro_rules! enum_subset {
     (
         $superset_enum:ident,
