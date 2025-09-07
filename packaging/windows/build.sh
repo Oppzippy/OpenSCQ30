d175296d27dce8b00c63d5cf49acc445ddf4d37a
Kyle Scheuing <kyle@kylescheuing.com>
2025-09-07 14:06:22 -0400

    build: create root dir for windows installer files

--- packaging/windows/build.sh
+++ packaging/windows/build.sh
@@ -6,6 +6,8 @@ script_dir="$(dirname -- "$script_path")"
 project_root="$script_dir/../.."
 input_exe="$project_root/target/release/openscq30-gui.exe"
 
+mkdir -p "$script_dir/root"
+
 cp "$input_exe" "$script_dir/root/"
 
 ISCC "$script_dir/setup.iss"
