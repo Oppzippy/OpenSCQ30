6e8d36874b49bb4d21725aeff3f83eaed382325c
Kyle Scheuing <kyle@kylescheuing.com>
2025-09-12 18:01:19 -0400

    refactor(gui): generate icon functions with macro

--- gui/src/icons.rs
+++ gui/src/icons.rs
@@ -1,16 +1,25 @@
 use cosmic::widget::icon::Handle;
 
-macro_rules! include_icon {
-    ($name:tt, $path:tt $(,)?) => {{
-        #[cfg(target_os = "linux")]
-        {
-            ::cosmic::widget::icon::from_name($name).handle()
-        }
-        #[cfg(not(target_os = "linux"))]
-        {
-            ::cosmic::widget::icon::from_svg_bytes(::std::include_bytes!($path)).symbolic(true)
-        }
-    }};
+macro_rules! icon {
+    ($($icon_name:tt as $function_name:ident),+ $(,)?) => {
+        $(
+            pub fn $function_name() -> Handle {
+                #[cfg(target_os = "linux")]
+                {
+                    ::cosmic::widget::icon::from_name($icon_name).handle()
+                }
+                #[cfg(not(target_os = "linux"))]
+                {
+                    ::cosmic::widget::icon::from_svg_bytes(::std::include_bytes!(concat!(
+                        "../icons/",
+                        $icon_name,
+                        ".svg"
+                    )))
+                    .symbolic(true)
+                }
+            }
+        )+
+    };
 }
 
 pub fn openscq30() -> Handle {
@@ -18,36 +27,12 @@ pub fn openscq30() -> Handle {
         .symbolic(false)
 }
 
-pub fn dialog_warning_symbolic() -> Handle {
-    include_icon!(
-        "dialog-warning-symbolic",
-        "../icons/dialog-warning-symbolic.svg"
-    )
-}
-
-pub fn edit_copy_symbolic() -> Handle {
-    include_icon!("edit-copy-symbolic", "../icons/edit-copy-symbolic.svg")
-}
-
-pub fn go_previous_symbolic() -> Handle {
-    include_icon!("go-previous-symbolic", "../icons/go-previous-symbolic.svg")
-}
-
-pub fn list_add_symbolic() -> Handle {
-    include_icon!("list-add-symbolic", "../icons/list-add-symbolic.svg")
-}
-
-pub fn list_remove_symbolic() -> Handle {
-    include_icon!("list-remove-symbolic", "../icons/list-remove-symbolic.svg")
-}
-
-pub fn view_refresh_symbolic() -> Handle {
-    include_icon!(
-        "view-refresh-symbolic",
-        "../icons/view-refresh-symbolic.svg"
-    )
-}
-
-pub fn help_about_symbolic() -> Handle {
-    include_icon!("help-about-symbolic", "../icons/help-about-symbolic.svg")
-}
+icon!(
+    "dialog-warning-symbolic" as dialog_warning_symbolic,
+    "edit-copy-symbolic" as edit_copy_symbolic,
+    "go-previous-symbolic" as go_previous_symbolic,
+    "list-add-symbolic" as list_add_symbolic,
+    "list-remove-symbolic" as list_remove_symbolic,
+    "view-refresh-symbolic" as view_refresh_symbolic,
+    "help-about-symbolic" as help_about_symbolic,
+);
