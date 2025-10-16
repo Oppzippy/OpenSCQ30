78791dfa65cef257c7c98b8f9b663d2419e53b9a
Kyle Scheuing <kyle@kylescheuing.com>
2025-10-16 00:48:26 -0400

    test(lib): use TestSoundcoreDevice for a3945 tests

--- lib/src/devices/soundcore/a3945.rs
+++ lib/src/devices/soundcore/a3945.rs
@@ -38,46 +38,25 @@ soundcore_device!(
 
 #[cfg(test)]
 mod tests {
-    use std::{sync::Arc, time::Duration};
-
-    use macaddr::MacAddr6;
-    use tokio::sync::mpsc;
+    use std::collections::HashMap;
 
     use crate::{
-        api::{
-            device::OpenSCQ30DeviceRegistry,
-            settings::{SettingId, Value},
-        },
-        connection_backend::mock::rfcomm::MockRfcommBackend,
+        api::settings::{SettingId, Value},
         devices::{
             DeviceModel,
             soundcore::common::{
-                packet::{
-                    self, Command, Direction, Packet,
-                    outbound::{OutboundPacket, OutboundPacketBytesExt},
-                },
+                device::test_utils::TestSoundcoreDevice,
+                packet::{self, Direction, Packet},
                 structures::{EqualizerConfiguration, PresetEqualizerProfile},
             },
         },
-        storage::OpenSCQ30Database,
     };
 
-    struct A3945TestStateUpdatePacket {
-        body: Vec<u8>,
-    }
-    impl OutboundPacket for A3945TestStateUpdatePacket {
-        fn command(&self) -> Command {
-            packet::inbound::STATE_COMMAND
-        }
-
-        fn body(&self) -> Vec<u8> {
-            self.body.to_owned()
-        }
-    }
-
-    #[tokio::test]
+    #[tokio::test(start_paused = true)]
     async fn it_remembers_band_9_and_10_values() {
-        let data = A3945TestStateUpdatePacket {
+        let state_update_packet = Packet {
+            direction: Direction::Inbound,
+            command: packet::inbound::STATE_COMMAND,
             body: vec![
                 0x01, // host device
                 0x00, // tws status
@@ -98,56 +77,32 @@ mod tests {
                 0x00, // bass up
                 0x00, // device color
             ],
-        }
-        .bytes();
+        };
 
-        let (inbound_sender, inbound_receiver) = mpsc::channel(10);
-        let (outbound_sender, mut outbound_receiver) = mpsc::channel(10);
-        let database = Arc::new(OpenSCQ30Database::new_in_memory().await.unwrap());
-        let registry = super::device_registry(
-            MockRfcommBackend::new(inbound_receiver, outbound_sender),
-            database,
+        let mut device = TestSoundcoreDevice::new_with_packet_responses(
             DeviceModel::SoundcoreA3945,
-        );
-        inbound_sender.send(data).await.unwrap();
-        let device = registry.connect(MacAddr6::nil()).await.unwrap();
-        _ = outbound_receiver
-            .recv()
-            .await
-            .expect("state update packet request");
+            super::device_registry,
+            HashMap::from([(packet::inbound::STATE_COMMAND, state_update_packet)]),
+        )
+        .await;
 
-        tokio::spawn(async move {
-            tokio::time::sleep(Duration::from_millis(10)).await;
-            inbound_sender
-                .send(
-                    Packet {
-                        direction: Direction::Inbound,
-                        command: packet::outbound::SET_EQUALIZER_COMMAND,
-                        body: Vec::new(),
-                    }
-                    .bytes(),
-                )
-                .await
-                .unwrap();
-        });
         device
-            .set_setting_values(vec![(
-                SettingId::PresetEqualizerProfile,
-                Value::OptionalString(Some("TrebleReducer".into())),
-            )])
-            .await
-            .unwrap();
-
-        let set_eq_packet_bytes = outbound_receiver.recv().await.unwrap();
-        assert_eq!(
-            Packet::from(packet::outbound::SetEqualizer {
-                equalizer_configuration: &EqualizerConfiguration::<2, 10>::new_from_preset_profile(
-                    PresetEqualizerProfile::TrebleReducer,
-                    [vec![1, 2], vec![3, 4]],
-                ),
-            })
-            .bytes(),
-            set_eq_packet_bytes
-        );
+            .assert_set_settings_response(
+                vec![(
+                    SettingId::PresetEqualizerProfile,
+                    Value::OptionalString(Some("TrebleReducer".into())),
+                )],
+                vec![
+                    packet::outbound::SetEqualizer {
+                        equalizer_configuration:
+                            &EqualizerConfiguration::<2, 10>::new_from_preset_profile(
+                                PresetEqualizerProfile::TrebleReducer,
+                                [vec![1, 2], vec![3, 4]],
+                            ),
+                    }
+                    .into(),
+                ],
+            )
+            .await;
     }
 }
