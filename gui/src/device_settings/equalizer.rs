77b97b8cb9458f01004742095323a080bb405fe1
Kyle Scheuing <kyle@kylescheuing.com>
2026-04-05 04:56:32 -0400

    refactor(lib): use libcosmic spin button
    This removes the db suffix from the number, but that's fine

--- gui/src/device_settings/equalizer.rs
+++ gui/src/device_settings/equalizer.rs
@@ -1,13 +1,4 @@
-use std::{
-    borrow::Cow,
-    ops::{Add, Sub},
-};
-
-use cosmic::{
-    Apply, Element,
-    iced::{Alignment, Border, Length, Shadow, alignment},
-    theme, widget,
-};
+use cosmic::{Element, iced::alignment, widget};
 use openscq30_lib::settings::Equalizer;
 
 use crate::fl;
@@ -29,14 +20,15 @@ where
             widget::column![
                 widget::text(fl!("hz", hz = hz)),
                 widget::row![
-                    wider_spin_button(
+                    widget::spin_button(
                         {
                             let divisor = 10i16.pow(setting.fraction_digits as u32);
                             let db_integer_portion = value[i] / divisor;
                             let db_decimal_portion = (value[i] % divisor).abs();
                             let decimal_db = format!("{db_integer_portion}.{db_decimal_portion}");
-                            fl!("db", db = decimal_db)
+                            decimal_db.to_string()
                         },
+                        fl!("hz", hz = hz),
                         value[i],
                         1,
                         setting.min,
@@ -45,7 +37,6 @@ where
                             let on_change = on_change.clone();
                             move |band_value| on_change(i as u8, band_value)
                         },
-                        80,
                     ),
                     widget::slider(
                         setting.min..=setting.max,
@@ -57,87 +48,12 @@ where
                             let on_change = on_change.clone();
                             move |band_value| on_change(i as u8, band_value)
                         },
-                    ),
+                    )
+                    .name(fl!("hz", hz = hz)),
                 ]
                 .align_y(alignment::Vertical::Center),
             ]
             .into()
         })
         .collect()
-}
-
-// Taken from libcosmic and modified to fix text not taking enough width, causing it to go to the next line
-fn wider_spin_button<'a, T, M>(
-    label: impl Into<Cow<'a, str>> + 'a,
-    value: T,
-    step: T,
-    min: T,
-    max: T,
-    on_press: impl Fn(T) -> M + 'static,
-    width: impl Into<Length>,
-) -> Element<'a, M>
-where
-    M: Clone + 'static,
-    T: Copy + Sub<Output = T> + Add<Output = T> + PartialOrd,
-{
-    let decrement_button = widget::icon::from_name("list-remove-symbolic")
-        .apply(widget::button::icon)
-        .on_press((on_press)(decrement::<T>(value, step, min, max)));
-
-    let increment_button = widget::icon::from_name("list-add-symbolic")
-        .clone()
-        .apply(widget::button::icon)
-        .on_press((on_press)(increment::<T>(value, step, min, max)));
-
-    let label = widget::text::body(label)
-        .apply(widget::container)
-        .width(width)
-        .align_x(Alignment::Center)
-        .align_y(Alignment::Center);
-
-    widget::row![decrement_button, label, increment_button]
-        .align_y(Alignment::Center)
-        .apply(widget::container)
-        .class(theme::Container::custom(container_style))
-        .into()
-}
-
-fn increment<T>(value: T, step: T, _min: T, max: T) -> T
-where
-    T: Copy + Sub<Output = T> + Add<Output = T> + PartialOrd,
-{
-    if value > max - step {
-        max
-    } else {
-        value + step
-    }
-}
-
-fn decrement<T>(value: T, step: T, min: T, _max: T) -> T
-where
-    T: Copy + Sub<Output = T> + Add<Output = T> + PartialOrd,
-{
-    if value < min + step {
-        min
-    } else {
-        value - step
-    }
-}
-
-fn container_style(theme: &cosmic::Theme) -> cosmic::iced::widget::container::Style {
-    let cosmic_theme = &theme.cosmic();
-    let accent = &cosmic_theme.accent;
-    let corners = &cosmic_theme.corner_radii;
-    cosmic::iced::widget::container::Style {
-        icon_color: Some(accent.base.into()),
-        text_color: Some(cosmic_theme.palette.neutral_10.into()),
-        background: None,
-        border: Border {
-            radius: corners.radius_s.into(),
-            width: 0.0,
-            color: accent.base.into(),
-        },
-        shadow: Shadow::default(),
-        snap: false,
-    }
 }
