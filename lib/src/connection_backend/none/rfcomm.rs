93a2d15f207e209d2ff8ea17730d380a3ae48051
Kyle Scheuing <kyle@kylescheuing.com>
2026-05-18 01:12:58 -0400

    refactor(lib): remove NoneRfcommConnection
    It's no longer necessary since NoneRfcommBackend doesn't use generics

--- lib/src/connection_backend/none/rfcomm.rs
+++ lib/src/connection_backend/none/rfcomm.rs
@@ -2,14 +2,12 @@ use std::{collections::HashSet, sync::Arc};
 
 use async_trait::async_trait;
 use macaddr::MacAddr6;
-use tokio::sync::{mpsc, watch};
 
 use crate::api::connection::{
     self, RfcommBackend, RfcommConnection, RfcommServiceSelectionStrategy,
 };
 
 pub struct NoneRfcommBackend;
-pub struct NoneRfcommConnection;
 
 #[async_trait]
 impl RfcommBackend for NoneRfcommBackend {
@@ -22,21 +20,6 @@ impl RfcommBackend for NoneRfcommBackend {
         _mac_address: MacAddr6,
         _select_uuid: RfcommServiceSelectionStrategy,
     ) -> connection::Result<Arc<dyn RfcommConnection + Send + Sync>> {
-        unimplemented!()
-    }
-}
-
-#[async_trait]
-impl RfcommConnection for NoneRfcommConnection {
-    async fn write(&self, _data: &[u8]) -> connection::Result<()> {
-        unimplemented!()
-    }
-
-    fn read_channel(&self) -> mpsc::Receiver<Vec<u8>> {
-        unimplemented!()
-    }
-
-    fn connection_status(&self) -> watch::Receiver<connection::ConnectionStatus> {
         unimplemented!()
     }
 }
