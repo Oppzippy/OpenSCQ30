62caa2b7b83abcfc7e241bb7cc2dead11cc87780
Kyle Scheuing <kyle@kylescheuing.com>
2026-05-17 23:17:56 -0400

    fix(lib): a3936 adaptive NC should be read only

--- lib/src/devices/soundcore/a3936/modules/sound_modes/setting_handler.rs
+++ lib/src/devices/soundcore/a3936/modules/sound_modes/setting_handler.rs
@@ -1,9 +1,10 @@
 use async_trait::async_trait;
+use openscq30_i18n::Translate;
 use openscq30_lib_has::Has;
 use strum::IntoEnumIterator;
 
 use crate::{
-    api::settings::{Range, Setting, SettingId, Value},
+    api::settings::{Setting, SettingId, Value},
     devices::soundcore::{
         a3936::structures::A3936SoundModes,
         common::settings_manager::{SettingHandler, SettingHandlerError, SettingHandlerResult},
@@ -27,7 +28,7 @@ where
 
     fn get(&self, state: &T, setting_id: &SettingId) -> Option<Setting> {
         let sound_modes = state.get();
-        let sound_mode_setting: SoundModeSetting = setting_id.try_into().ok()?;
+        let sound_mode_setting: SoundModeSetting = (*setting_id).try_into().ok()?;
         Some(match sound_mode_setting {
             SoundModeSetting::AmbientSoundMode => {
                 Setting::select_from_enum_all_variants(sound_modes.ambient_sound_mode)
@@ -38,9 +39,10 @@ where
             SoundModeSetting::NoiseCancelingMode => {
                 Setting::select_from_enum_all_variants(sound_modes.noise_canceling_mode)
             }
-            SoundModeSetting::AdaptiveNoiseCanceling => {
-                Setting::select_from_enum_all_variants(sound_modes.adaptive_noise_canceling)
-            }
+            SoundModeSetting::AdaptiveNoiseCanceling => Setting::Information {
+                value: sound_modes.adaptive_noise_canceling.to_string(),
+                translated_value: sound_modes.adaptive_noise_canceling.translate(),
+            },
             SoundModeSetting::ManualNoiseCanceling => {
                 Setting::select_from_enum_all_variants(sound_modes.manual_noise_canceling)
             }
@@ -55,15 +57,6 @@ where
                     fl!("no")
                 },
             },
-            SoundModeSetting::AdaptiveNoiseCancelingSensitivityLevel => Setting::I32Range {
-                setting: Range {
-                    range: 0..=10,
-                    step: 1,
-                },
-                value: sound_modes
-                    .noise_canceling_adaptive_sensitivity_level
-                    .into(),
-            },
         })
     }
 
@@ -74,7 +67,7 @@ where
         value: Value,
     ) -> SettingHandlerResult<()> {
         let sound_modes = state.get_mut();
-        let sound_mode_setting: SoundModeSetting = setting_id
+        let sound_mode_setting: SoundModeSetting = (*setting_id)
             .try_into()
             .expect("already filtered to valid values only by SettingsManager");
         match sound_mode_setting {
@@ -87,9 +80,7 @@ where
             SoundModeSetting::NoiseCancelingMode => {
                 sound_modes.noise_canceling_mode = value.try_as_enum_variant()?;
             }
-            SoundModeSetting::AdaptiveNoiseCanceling => {
-                sound_modes.adaptive_noise_canceling = value.try_as_enum_variant()?;
-            }
+            SoundModeSetting::AdaptiveNoiseCanceling => return Err(SettingHandlerError::ReadOnly),
             SoundModeSetting::ManualNoiseCanceling => {
                 sound_modes.manual_noise_canceling = value.try_as_enum_variant()?;
             }
@@ -97,9 +88,6 @@ where
                 sound_modes.wind_noise.is_suppression_enabled = value.try_as_bool()?;
             }
             SoundModeSetting::WindNoiseDetected => return Err(SettingHandlerError::ReadOnly),
-            SoundModeSetting::AdaptiveNoiseCancelingSensitivityLevel => {
-                sound_modes.noise_canceling_adaptive_sensitivity_level = value.try_as_i32()? as u8;
-            }
         }
         Ok(())
     }
