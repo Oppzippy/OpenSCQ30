Oppzippy/OpenSCQ30

commit 167c56a8156d91066895e420d2acd38a9522f2c3

fix(lib): remove a3949 custom equalizer profiles
The a3949 does not support custom profiles, so remoeve the setting for
it. Since we still want to show VolumeAdjustments, the Equalizer setting
type can now be read only.

lib/src/devices/soundcore/common/modules/equalizer/setting_handler.rs
@@ -13,7 +13,7 @@ use crate::{
             EqualizerModuleSettings, InvisibleBandsMode,
             custom_equalizer_profile_store::CustomEqualizerProfileStore,
         },
-        settings_manager::{SettingHandler, SettingHandlerResult},
+        settings_manager::{SettingHandler, SettingHandlerError, SettingHandlerResult},
         structures::{EqualizerConfiguration, TwsStatus, VolumeAdjustments},
     },
 };
@@ -225,6 +225,9 @@ fn get_inner<
             }
         }
         EqualizerSetting::CustomEqualizerProfile => {
+            let Some(custom_preset_id) = module_settings.custom_preset_id else {
+                return None;
+            };
             let custom_profiles = custom_profiles_receiver.borrow();
             Setting::ModifiableSelect {
                 setting: {
@@ -239,7 +242,7 @@ fn get_inner<
                             .collect(),
                     }
                 },
-                value: (equalizer_configuration.preset_id() == module_settings.custom_preset_id)
+                value: (equalizer_configuration.preset_id() == custom_preset_id)
                     .then(|| {
                         custom_profiles
                             .iter()
@@ -263,6 +266,7 @@ fn get_inner<
                 min: MIN_VOLUME,
                 max: MAX_VOLUME,
             },
+            read_only: module_settings.custom_preset_id.is_none(),
             value: equalizer_configuration
                 .volume_adjustments_channel_1()
                 .copied()
@@ -323,14 +327,13 @@ async fn set_inner<
                         &module_settings.invisible_bands_mode,
                     ),
                 );
-            } else {
-                *equalizer_configuration = EqualizerConfiguration::new(
-                    module_settings.custom_preset_id,
-                    *equalizer_configuration.volume_adjustments(),
-                );
             }
         }
         EqualizerSetting::CustomEqualizerProfile => {
+            let Some(custom_preset_id) = module_settings.custom_preset_id else {
+                // TODO return error
+                return Ok(());
+            };
             if let Ok(name) = value.try_as_str() {
                 if let Some(volume_adjustments) = custom_profiles_receiver
                     .borrow()
@@ -351,7 +354,7 @@ async fn set_inner<
                         .collect_array()
                         .expect("we made sure there are exactly VISIBLE_BANDS elements");
                     *equalizer_configuration = EqualizerConfiguration::new_all_bands_present(
-                        module_settings.custom_preset_id,
+                        custom_preset_id,
                         values_to_volume_adjustments(
                             &fixed_len_volume_adjustments,
                             &module_settings.invisible_bands_mode,
@@ -383,9 +386,14 @@ async fn set_inner<
             }
         }
         EqualizerSetting::VolumeAdjustments => {
+            let Some(custom_preset_id) = module_settings.custom_preset_id else {
+                // We want to display the equalizer but not accept any changes if custom profiles aren't supported,
+                // so don't error, just don't make any changes.
+                return Err(SettingHandlerError::ReadOnly);
+            };
             let volume_adjustments = value.try_as_i16_array::<VISIBLE_BANDS>()?;
             *equalizer_configuration = EqualizerConfiguration::new_all_bands_present(
-                module_settings.custom_preset_id,
+                custom_preset_id,
                 values_to_volume_adjustments(
                     volume_adjustments,
                     &module_settings.invisible_bands_mode,

android/app/src/main/java/com/oppzippy/openscq30/lib/wrapper/Setting.kt
@@ -52,7 +52,7 @@ sealed class Setting {
 
     @Serializable
     @SerialName("equalizer")
-    data class EqualizerSetting(val setting: Equalizer, val value: List<Short>) : Setting() {
+    data class EqualizerSetting(val setting: Equalizer, val readOnly: Boolean, val value: List<Short>) : Setting() {
         override fun toValue() = value.toValue()
     }
 

android/app/src/main/java/com/oppzippy/openscq30/ui/devicesettings/composables/ReadOnlyEqualizer.kt
@@ -1,0 +1,113 @@
+package com.oppzippy.openscq30.ui.devicesettings.composables
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.oppzippy.openscq30.R
+import com.oppzippy.openscq30.ui.theme.OpenSCQ30Theme
+import java.math.BigDecimal
+import java.math.MathContext
+import kotlin.math.absoluteValue
+
+@Composable
+fun ReadOnlyEqualizer(
+    modifier: Modifier,
+    bands: List<UShort>,
+    values: List<Short>,
+    minValue: Short,
+    maxValue: Short,
+    fractionDigits: Short,
+) {
+    val contentDescription = bands.take(values.size).map { bandHz ->
+        if (bandHz < 1000.toUShort()) {
+            stringResource(R.string.hz, bandHz.toInt())
+        } else {
+            stringResource(
+                R.string.khz,
+                BigDecimal(bandHz.toInt()).scaleByPowerOfTen(-3).round(MathContext(1)).toPlainString(),
+            )
+        }
+    }.withIndex().joinToString(separator = ", ") { (index, bandHz) ->
+        "$bandHz: ${BigDecimal(values[index].toInt()).scaleByPowerOfTen(-fractionDigits)}"
+    }
+
+    val color = MaterialTheme.colorScheme.onBackground
+
+    Surface {
+        Canvas(modifier, contentDescription) {
+            val strokeWidth = size.height / 25f
+            val padding = Offset(strokeWidth, strokeWidth)
+            val sizeWithoutPadding = Offset(
+                x = size.width - padding.x * 2f,
+                y = size.height - padding.y * 2f,
+            )
+
+            val range = (maxValue - minValue).toFloat()
+            val zeroValueY = size.height - size.height * (minValue.toFloat().absoluteValue / range)
+
+            listOf(strokeWidth, zeroValueY, size.height - strokeWidth).forEach { y ->
+                drawLine(
+                    color = color.copy(alpha = 0.3f),
+                    start = Offset(0f, y),
+                    end = Offset(size.width, y),
+                    strokeWidth = strokeWidth,
+                )
+            }
+
+            val points = linePoints(
+                minValue = minValue,
+                maxValue = maxValue,
+                values = values,
+            )
+            points.zipWithNext().forEach { (left, right) ->
+                drawLine(
+                    color = color,
+                    start = left * sizeWithoutPadding + padding,
+                    end = right * sizeWithoutPadding + padding,
+                    strokeWidth = strokeWidth,
+                    cap = StrokeCap.Round,
+                )
+            }
+        }
+    }
+}
+
+private fun linePoints(minValue: Short, maxValue: Short, values: List<Short>): List<Offset> {
+    val range = (maxValue - minValue).toFloat()
+    return values.mapIndexed { index, value ->
+        val x = index.toFloat() / (values.size - 1).toFloat()
+        val y = (value - minValue).toFloat() / range
+        Offset(x, 1 - y)
+    }
+}
+
+private operator fun Offset.times(other: Offset): Offset = Offset(x * other.x, y * other.y)
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewEqualizer() {
+    val bands = listOf<UShort>(100u, 200u, 400u, 800u, 1600u, 3200u, 6400u, 12000u)
+    OpenSCQ30Theme {
+        Surface {
+            ReadOnlyEqualizer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp),
+                bands = bands,
+                values = listOf(60, -60, 30, -30, 120, -120, 0, 0),
+                minValue = -120,
+                maxValue = 134,
+                fractionDigits = 1,
+            )
+        }
+    }
+}

android/app/src/main/java/com/oppzippy/openscq30/ui/devicesettings/screens/SettingCategoryScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -46,6 +47,7 @@ import com.oppzippy.openscq30.lib.wrapper.Setting
 import com.oppzippy.openscq30.lib.wrapper.Value
 import com.oppzippy.openscq30.lib.wrapper.toValue
 import com.oppzippy.openscq30.ui.devicesettings.composables.Equalizer
+import com.oppzippy.openscq30.ui.devicesettings.composables.ReadOnlyEqualizer
 import com.oppzippy.openscq30.ui.devicesettings.screens.categoryoverrides.categoryScreenOverrides
 import com.oppzippy.openscq30.ui.utils.Labeled
 import com.oppzippy.openscq30.ui.utils.LabeledSwitch
@@ -225,24 +227,37 @@ private fun I32Range(name: String, range: Range<Int>, value: Int, onChange: (Int
 
 @Composable
 private fun Equalizer(name: String, setting: Setting.EqualizerSetting, onChange: (List<Short>) -> Unit) {
-    Equalizer(
-        bands = setting.setting.bandHz,
-        values = setting.value,
-        minValue = setting.setting.min,
-        maxValue = setting.setting.max,
-        fractionDigits = setting.setting.fractionDigits,
-        onValueChange = { changedIndex, newValue ->
-            onChange(
-                setting.value.mapIndexed { index, oldValue ->
-                    if (index == changedIndex) {
-                        newValue
-                    } else {
-                        oldValue
-                    }
-                },
-            )
-        },
-    )
+    if (!setting.readOnly) {
+        Equalizer(
+            bands = setting.setting.bandHz,
+            values = setting.value,
+            minValue = setting.setting.min,
+            maxValue = setting.setting.max,
+            fractionDigits = setting.setting.fractionDigits,
+            onValueChange = { changedIndex, newValue ->
+                onChange(
+                    setting.value.mapIndexed { index, oldValue ->
+                        if (index == changedIndex) {
+                            newValue
+                        } else {
+                            oldValue
+                        }
+                    },
+                )
+            },
+        )
+    } else {
+        ReadOnlyEqualizer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp),
+            bands = setting.setting.bandHz,
+            values = setting.value,
+            minValue = setting.setting.min,
+            maxValue = setting.setting.max,
+            fractionDigits = setting.setting.fractionDigits,
+        )
+    }
 }
 
 @Composable

gui/src/device_settings.rs
@@ -14,7 +14,7 @@ use std::{borrow::Cow, collections::HashMap, path::PathBuf};
 use cosmic::{
     Element, Task,
     app::context_drawer::ContextDrawer,
-    iced::keyboard,
+    iced::{Length, keyboard},
     widget::{self, menu::KeyBind, nav_bar},
 };
 use legacy_migration::LegacyMigrationModel;
@@ -28,7 +28,7 @@ use tracing::{Instrument, debug};
 
 use crate::{
     app::DebugOpenSCQ30Device,
-    fl, handle_soft_error,
+    equalizer_line, fl, handle_soft_error,
     openscq30_v1_migration::{self, LegacyEqualizerProfile},
     throttle,
     utils::coalesce_result,
@@ -422,11 +422,28 @@ impl DeviceSettingsModel {
                 },
             )
             .into(),
-            Setting::Equalizer { setting, value } => {
-                equalizer::horizontal_equalizer(setting, value, move |index, value| {
-                    Message::SetEqualizerBand(setting_id, index, value)
-                })
-                .into()
+            Setting::Equalizer {
+                setting,
+                read_only,
+                value,
+            } => {
+                if !read_only {
+                    equalizer::horizontal_equalizer(setting, value, move |index, value| {
+                        Message::SetEqualizerBand(setting_id, index, value)
+                    })
+                    .into()
+                } else {
+                    // TODO reuse EqualizerLine so that the canvas cache is in effect
+                    Element::from(
+                        widget::canvas(equalizer_line::EqualizerLine::new(
+                            setting.min,
+                            setting.max,
+                            value.to_vec(),
+                        ))
+                        .width(Length::Fill),
+                    )
+                    .into()
+                }
             }
             Setting::Information {
                 value: _,
@@ -508,6 +525,7 @@ impl DeviceSettingsModel {
             Message::SetEqualizerBand(setting_id, index, new_value) => {
                 if let Some(Setting::Equalizer {
                     setting: _,
+                    read_only: _,
                     value: values,
                 }) = self.throttle.setting(&setting_id)
                 {

lib/src/api/settings.rs
@@ -210,6 +210,7 @@ pub enum Setting {
     },
     Equalizer {
         setting: Equalizer,
+        read_only: bool,
         value: Vec<i16>,
     },
     Information {

lib/src/devices/soundcore/a3116/modules/equalizer.rs
@@ -43,7 +43,7 @@ where
 
 pub fn module_settings() -> EqualizerModuleSettings<9, 9, -6, 6, 0> {
     EqualizerModuleSettings {
-        custom_preset_id: 0xf,
+        custom_preset_id: Some(0xf),
         band_hz: [80, 150, 300, 500, 700, 1000, 5000, 8000, 12000],
         invisible_bands_mode: InvisibleBandsMode::Fixed(Vec::new()),
         presets: vec![

lib/src/devices/soundcore/a3949.rs
@@ -28,7 +28,10 @@ soundcore_device!(
     async |builder| {
         builder.module_collection().add_state_update();
         builder
-            .equalizer_with_drc_tws(common::modules::equalizer::common_settings_type_2())
+            .equalizer_with_drc_tws(common::modules::equalizer::EqualizerModuleSettings {
+                custom_preset_id: None, // device doesn't support custom presets
+                ..common::modules::equalizer::common_settings_type_2()
+            })
             .await;
 
         builder.button_configuration(&BUTTON_CONFIGURATION_SETTINGS);

commit ce57c7aa6978d06561805962023eb6abc56add6c

fix(lib): setting PresetEqualizerProfile to None should switch to custom
This is a regression introduced in
167c56a8156d91066895e420d2acd38a9522f2c3.

lib/src/devices/soundcore/common/modules/equalizer/setting_handler.rs
@@ -346,6 +346,17 @@ async fn set_inner<
                         &module_settings.invisible_bands_mode,
                     ),
                 );
+            } else if let Some(custom_preset_id) = module_settings.custom_preset_id
+                && let Some(volume_adjustments) =
+                    equalizer_configuration.volume_adjustments_channel_1()
+            {
+                *equalizer_configuration = EqualizerConfiguration::new_all_bands_present(
+                    custom_preset_id,
+                    values_to_volume_adjustments(
+                        volume_adjustments.adjustments(),
+                        &module_settings.invisible_bands_mode,
+                    ),
+                )
             }
         }
         EqualizerSetting::CustomEqualizerProfile => {
@@ -616,5 +627,32 @@ mod tests {
         };
 
         assert_eq!(custom_profile, Some(Cow::from("test profile")));
+    }
+
+    #[tokio::test(start_paused = true)]
+    async fn switching_to_none_preset_keeps_volume_adjustments_as_is_but_changes_preset_id() {
+        let (_, setting_handler, _) = set_up().await;
+        let mut state = TestStateWithEq {
+            equalizer_configuration: EqualizerConfiguration::new_all_bands_present(
+                0x0000,
+                [VolumeAdjustments::new([0; 10]); 2],
+            ),
+        };
+        setting_handler
+            .set(
+                &mut state,
+                &SettingId::PresetEqualizerProfile,
+                Value::OptionalString(None),
+            )
+            .await
+            .unwrap();
+        let Setting::PresetEqualizerProfileSelect { value, .. } = setting_handler
+            .get(&mut state, &SettingId::PresetEqualizerProfile)
+            .unwrap()
+        else {
+            panic!("setting type should be PresetEqualizerProfileSelect")
+        };
+        assert_eq!(value, None);
+        assert_eq!(state.equalizer_configuration.preset_id(), 0xfefe);
     }
 }